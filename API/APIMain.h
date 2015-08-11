4cd3e6ca1847bb9eeb0bf86df8cfa59c137635e1
0x4D1 <kaos4d1@gmail.com>
2015-08-11 00:59:06 -0700

    Cute little title page thing in api for credits and stuff. Official name for the api is gonna be GWCA++, GWAPI is used too much lol

--- API/APIMain.h
+++ API/APIMain.h
@@ -3,6 +3,56 @@
 
 #include <Windows.h>
 
+/**********************************************************************************\
+
+		 _____ _    _ _____   ___              
+		|  __ \ |  | /  __ \ / _ \   _     _   
+		| |  \/ |  | | /  \// /_\ \_| |_ _| |_ 
+		| | __| |/\| | |    |  _  |_   _|_   _|
+		| |_\ \  /\  / \__/\| | | | |_|   |_|  
+		 \____/\/  \/ \____/\_| |_/            
+                                
+
+		Created by KAOS (a.k.a. 4D 1) for use in GWToolbox++
+		
+		Credits to:
+		
+		Sune C (a.k.a. Harboe) and ACB - 
+			Most low level ground work of API via the GWCA and GWLP:R 
+			projects as well as getting the gamerevision community rolling.
+			Much help from following these two gentlemen's breadcrumbs.
+			
+		Miracle444 -
+			GWA2, as well as helping me really understand how the game handles
+			its memory information.
+			
+		d8rken - 
+			Zraw menu/api which acted as a gateway to get a grasp of cpp as well
+			as directx.
+			
+		Has - 
+			For getting me to not make everything singletons :P
+		
+   Permission is hereby granted, free of charge, to any person obtaining a copy
+   of this software and associated documentation files (the "Software"), to deal
+   in the Software without restriction, including without limitation the rights
+   to use, copy, modify, and/or merge copies of the Software, and to permit 
+   persons to whom the Software is furnished to do so, subject to the 
+   following conditions:
+  
+   The above copyright notice and this permission notice shall be included in
+   all copies or substantial portions of the Software.
+  
+   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+   IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+   FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+   AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+   LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+   OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+   THE SOFTWARE.
+
+\**********************************************************************************/
+
 // Comment out this #define to make the directx shit not compile into the project.
 #define GWAPI_USEDIRECTX
 
