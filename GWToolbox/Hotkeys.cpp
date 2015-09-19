748b51bcdca57c14edd98ccddcdb021415f238ca
0x4D1 <kaos4d1@gmail.com>
2015-09-18 19:24:41 -0700

    - Set stol in dialog hotkey event to autodetect number base given prefixes so i can use hex ids lol

--- GWToolbox/Hotkeys.cpp
+++ GWToolbox/Hotkeys.cpp
@@ -493,7 +493,7 @@ HotkeyDialog::HotkeyDialog(Key key, Key modifier, bool active, wstring ini_secti
 	id_box->GetTextChangedEvent() += TextChangedEventHandler(
 		[this, id_box, ini_section](Control*) {
 		try {
-			long id = std::stol(id_box->GetText());
+			long id = std::stol(id_box->GetText(),0,0);
 			this->set_id((UINT)id);
 			GWToolbox::instance()->config()->iniWriteLong(ini_section.c_str(),
 				this->IniKeyDialogID(), id);
