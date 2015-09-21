fb09f08cfc73dd083e054a48a4b5cebdb66064eb
0x4D1 <kaos4d1@gmail.com>
2015-09-20 22:46:19 -0700

    - Added basic UEH to start off with, can be helped further as we go

--- main.cpp
+++ main.cpp
@@ -1,7 +1,48 @@
 #include "API\APIMain.h"
 #include "GWToolbox\GWToolbox.h"
 
+#include <ctime>
+#include <fstream>
+
 #define DEBUG 1
+
+LONG WINAPI ExceptionHandler(EXCEPTION_POINTERS* ExceptionInfo)
+{
+	string filename;
+	std::time_t time = std::time(nullptr);
+	filename += GuiUtils::getSubPathA(std::asctime(std::localtime(&time)), "crash");
+	filename += ".txt";
+	ofstream logfile(filename.c_str());
+
+	PEXCEPTION_RECORD records = ExceptionInfo->ExceptionRecord;
+	PCONTEXT cpudbg = ExceptionInfo->ContextRecord;
+
+	logfile << "Code: " << records->ExceptionCode << endl;
+	logfile << "Addr: " << records->ExceptionAddress << endl;
+	logfile << "Flag: " << records->ExceptionFlags << endl << endl;
+
+	logfile << "Registers:" << endl;
+	logfile << "\teax:" << cpudbg->Eax << endl;
+	logfile << "\tebx:" << cpudbg->Ebx << endl;
+	logfile << "\tecx:" << cpudbg->Ecx << endl;
+	logfile << "\tedx:" << cpudbg->Edx << endl;
+	logfile << "\tesi:" << cpudbg->Esi << endl;
+	logfile << "\tedi:" << cpudbg->Edi << endl;
+	logfile << "\tesp:" << cpudbg->Esp << endl;
+	logfile << "\tebp:" << cpudbg->Ebp << endl;
+	logfile << "\teip:" << cpudbg->Eip << endl << endl;
+
+	logfile << "Stack:" << endl;
+
+	for (DWORD i = 0; i <= 0x40; i += 4)
+		logfile << "\t[esp+" << i << "]: " << ((DWORD*)(cpudbg->Esp))[i / 4];
+
+
+	logfile.close();
+
+	MessageBoxA(0, "Guild Wars - Crash!", "GWToolbox has detected Guild Wars has crashed, oops.\nThis may be a crash toolbox has made, or something completely irrelevant to it.\nFor help on this matter, please access the log file placed in the \"crash\" folder of the GWToolbox data folder with the correct date and time, and send the file along with any information or actions surrounding the crash to the GWToolbox++ developers.\nThank you and sorry for the inconvenience.", 0);
+	return EXCEPTION_EXECUTE_HANDLER;
+}
 
 // Do all your startup things here instead.
 void init(HMODULE hModule){
@@ -14,6 +55,7 @@ void init(HMODULE hModule){
 	SetConsoleTitleA("GWTB++ Debug Console");
 #endif
 
+	SetUnhandledExceptionFilter(ExceptionHandler);
 	CreateThread(0, 0, (LPTHREAD_START_ROUTINE)GWToolbox::threadEntry, hModule, 0, 0);
 }
 
