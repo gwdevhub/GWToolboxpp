35a4f4a5e8cd24434784b9d8a093094f19a7fa6c
0x4D1 <kaos4d1@gmail.com>
2015-09-21 21:04:54 -0700

    - Ignore compiled binary exe (change this if you want)  - Set numbers to be read in hex for exception handler.  - Handler will try to continue execution if it is not a critical error

--- main.cpp
+++ main.cpp
@@ -8,40 +8,47 @@
 
 LONG WINAPI ExceptionHandler(EXCEPTION_POINTERS* ExceptionInfo)
 {
-	string filename;
-	std::time_t time = std::time(nullptr);
-	filename += GuiUtils::getSubPathA(std::asctime(std::localtime(&time)), "crash");
-	filename += ".txt";
-	ofstream logfile(filename.c_str());
-
 	PEXCEPTION_RECORD records = ExceptionInfo->ExceptionRecord;
 	PCONTEXT cpudbg = ExceptionInfo->ContextRecord;
-
-	logfile << "Code: " << records->ExceptionCode << endl;
-	logfile << "Addr: " << records->ExceptionAddress << endl;
-	logfile << "Flag: " << records->ExceptionFlags << endl << endl;
-
-	logfile << "Registers:" << endl;
-	logfile << "\teax:" << cpudbg->Eax << endl;
-	logfile << "\tebx:" << cpudbg->Ebx << endl;
-	logfile << "\tecx:" << cpudbg->Ecx << endl;
-	logfile << "\tedx:" << cpudbg->Edx << endl;
-	logfile << "\tesi:" << cpudbg->Esi << endl;
-	logfile << "\tedi:" << cpudbg->Edi << endl;
-	logfile << "\tesp:" << cpudbg->Esp << endl;
-	logfile << "\tebp:" << cpudbg->Ebp << endl;
-	logfile << "\teip:" << cpudbg->Eip << endl << endl;
-
-	logfile << "Stack:" << endl;
-
-	for (DWORD i = 0; i <= 0x40; i += 4)
-		logfile << "\t[esp+" << i << "]: " << ((DWORD*)(cpudbg->Esp))[i / 4];
+	if (records->ExceptionFlags != 0){
+		string filename;
+		std::time_t time = std::time(nullptr);
+		filename += GuiUtils::getSubPathA(std::asctime(std::localtime(&time)), "crash");
+		filename += ".txt";
+		ofstream logfile(filename.c_str());
 
 
-	logfile.close();
 
-	MessageBoxA(0, "Guild Wars - Crash!", "GWToolbox has detected Guild Wars has crashed, oops.\nThis may be a crash toolbox has made, or something completely irrelevant to it.\nFor help on this matter, please access the log file placed in the \"crash\" folder of the GWToolbox data folder with the correct date and time, and send the file along with any information or actions surrounding the crash to the GWToolbox++ developers.\nThank you and sorry for the inconvenience.", 0);
-	return EXCEPTION_EXECUTE_HANDLER;
+		logfile << hex;
+		logfile << "Code: " << records->ExceptionCode << endl;
+		logfile << "Addr: " << records->ExceptionAddress << endl;
+		logfile << "Flag: " << records->ExceptionFlags << endl << endl;
+
+		logfile << "Registers:" << endl;
+		logfile << "\teax: " << cpudbg->Eax << endl;
+		logfile << "\tebx: " << cpudbg->Ebx << endl;
+		logfile << "\tecx: " << cpudbg->Ecx << endl;
+		logfile << "\tedx: " << cpudbg->Edx << endl;
+		logfile << "\tesi: " << cpudbg->Esi << endl;
+		logfile << "\tedi: " << cpudbg->Edi << endl;
+		logfile << "\tesp: " << cpudbg->Esp << endl;
+		logfile << "\tebp: " << cpudbg->Ebp << endl;
+		logfile << "\teip: " << cpudbg->Eip << endl << endl;
+
+		logfile << "Stack:" << endl;
+
+		for (DWORD i = 0; i <= 0x40; i += 4)
+			logfile << "\t[esp+" << i << "]: " << ((DWORD*)(cpudbg->Esp))[i / 4];
+
+
+		logfile.close();
+
+		MessageBoxA(0, "Guild Wars - Crash!", "GWToolbox has detected Guild Wars has crashed, oops.\nThis may be a crash toolbox has made, or something completely irrelevant to it.\nFor help on this matter, please access the log file placed in the \"crash\" folder of the GWToolbox data folder with the correct date and time, and send the file along with any information or actions surrounding the crash to the GWToolbox++ developers.\nThank you and sorry for the inconvenience.", 0);
+		return EXCEPTION_EXECUTE_HANDLER;
+	}
+	else{
+		return EXCEPTION_CONTINUE_EXECUTION;
+	}
 }
 
 // Do all your startup things here instead.
