d7fa31445f15b46cce42396ed52b58cdaee5b921
Giorgio Gori <gorig@usi.ch>
2015-09-29 23:25:33 -0700

    new way to get d3d9 module

--- API/DirectXMgr.cpp
+++ API/DirectXMgr.cpp
@@ -1,5 +1,7 @@
 #include "DirectXMgr.h"
 #include "..\GWToolbox\logger.h"
+#include <Windows.h>
+#include <Psapi.h>
 
 GWAPI::DirectXMgr::DirectXMgr(GWAPIMgr* obj) : parent_(obj)
 {
@@ -22,26 +24,58 @@ void GWAPI::DirectXMgr::CreateRenderHooks(EndScene_t _endscene, Reset_t _reset)
 
 	LOG("DX Start\n");
 
-	HMODULE hD3D9 = NULL;
-	while (!hD3D9)
-	{
-		hD3D9 = GetModuleHandleA("d3d9.dll");
-		Sleep(5);
+	HMODULE hMods[1024];
+	HANDLE hProcess = GetCurrentProcess();
+	DWORD cbNeeded;
+	if (!EnumProcessModules(hProcess, hMods, sizeof(hMods), &cbNeeded)) {
+		LOG("EnumProcessModules failed, error %d\n", GetLastError());
+		return;
 	}
 
-	LOG("DX Module found! Addr: %X\n", hD3D9);
+	DWORD pA1 = NULL;
+	for (int i = 0; i < (cbNeeded / sizeof(HMODULE)); ++i) {
+		TCHAR mod_name[MAX_PATH];
+		if (!GetModuleBaseName(hProcess, hMods[i], mod_name, MAX_PATH)) {
+			LOG("GetModuleBaseName failed, error %X\n", GetLastError());
+			continue;
+		}
 
-	DWORD pA1 = FindPattern((DWORD)hD3D9, 0x128000, "\xC7\x06\x00\x00\x00\x00\x89\x86\x00\x00\x00\x00\x89\x86", "xx????xx????xx");
-	LOG("Vtable scan found! Addr: %X\n", pA1);
+		if (wcscmp(mod_name, L"d3d9.dll") != 0) continue;
+
+		LOG("Found directx! name = %ls, handle = %X\n", mod_name, hMods[i]);
+
+		MODULEINFO info;
+		if (!GetModuleInformation(hProcess, hMods[i], &info, sizeof(MODULEINFO))) {
+			LOG("GetModuleInformation failed, error %X\n", GetLastError());
+			continue;
+		}
+		DWORD start = (DWORD)info.lpBaseOfDll;
+		DWORD size = info.SizeOfImage;
+
+		LOG("Module info: start %X, size %X\n", start, size);
+
+		DWORD scan_size = 0x128000;
+		if (size < scan_size) continue;
+
+		pA1 = FindPattern(start, scan_size, 
+			"\xC7\x06\x00\x00\x00\x00\x89\x86\x00\x00\x00\x00\x89\x86", "xx????xx????xx");
+		if (pA1 != NULL) {
+			LOG("Vtable scan found! Addr: %X\n", pA1);
+			break;
+		} else {
+			LOG("Scanned a module but could not find pattern!\n");
+		}
+	}
+
 	memcpy(&vtable_start_, (void*)(pA1 + 2), 4);
 	LOG("memcpy vtable done\n");
 	DWORD dwEndsceneLen = Hook::CalculateDetourLength((BYTE*)(vtable_start_[42]));
 	endscene_ = (EndScene_t)hk_endscene_.Detour((BYTE*)(vtable_start_[42]), (BYTE*)_endscene, dwEndsceneLen);
-	LOG("DX endscene! Len: %d Addr: %X\n", dwEndsceneLen, hD3D9);
+	LOG("DX endscene! Len: %d\n", dwEndsceneLen);
 
 	DWORD dwResetLen = Hook::CalculateDetourLength((BYTE*)(vtable_start_[16]));
 	reset_ = (Reset_t)hk_reset_.Detour((BYTE*)(vtable_start_[16]), (BYTE*)_reset, dwResetLen);
-	LOG("DX reset! Len: %d Addr: %X\n", hD3D9);
+	LOG("DX reset! Len: %d\n", dwResetLen);
 
 	hooked_ = true;
 }
@@ -58,16 +92,7 @@ void GWAPI::DirectXMgr::RestoreHooks()
 
 DWORD GWAPI::DirectXMgr::FindPattern(DWORD _base, DWORD _size, char* _pattern, char* _mask)
 {
-
-	LOG("Finding pattern Base: %X Size: %X\n", _base, _size);
 	int pattern_length = strlen(_mask);
-
-	LOG("Pattern: \n");
-
-	for (int i = 0; i < pattern_length; i++) {
-		printf("%02X \n", _pattern[i]);
-		Logger::FlushFile();
-	}
 	
 	bool found = false;
 
