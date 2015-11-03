81771291c70021de3640bad36af89acec010a58e
Giorgio Gori <gorig@usi.ch>
2015-11-02 19:13:30 -0800

    very early draft of command parser

--- GWToolbox/GWToolbox/ChatCommands.cpp
+++ GWToolbox/GWToolbox/ChatCommands.cpp
@@ -6,35 +6,67 @@
 using namespace std;
 
 ChatCommands::ChatCommands() {
-	GWCA api;
+	GWCA::Api().Chat().RegisterChannel(L"GWToolbox++", 0x00CCFF, 0xDDDDDD);
 
-	api().Chat().RegisterChannel(L"GWToolbox++", 0x00CCFF, 0xDDDDDD);
-
-	api().Chat().RegisterCommand(L"pcons", 
-		std::bind(&ChatCommands::PconCmd, this, std::placeholders::_1));
-
-	api().Chat().RegisterCommand(L"age2", [api](vector<wstring>&){ // Exemple of new channel system
+	AddCommand(L"age2", [](vector<wstring>) {
 		wchar_t buffer[30];
+		GWCA api;
 		DWORD second = api().Map().GetInstanceTime() / 1000;
-
 		wsprintf(buffer, L"%02u:%02u:%02u", (second / 3600), (second / 60) % 60, second % 60);
-
 		api().Chat().WriteChat(L"GWToolbox++", buffer);
+		return true;
+	});
+
+	AddCommand(L"pcons", std::bind(&ChatCommands::PconCmd, this, std::placeholders::_1));
+
+	// sleep ms
+	// waits for ms milliseconds
+	AddCommand(L"sleep", [](vector<wstring> args) {
+		if (args.size() < 1) {
+			printf("sleep requires one argument (time in milliseconds)");
+			return false;
+		}
+		try {
+			long time = std::stoi(args[0]);
+			Sleep(time);
+			return true;
+		} catch (std::invalid_argument e) {
+			printf("sleep requires an integer as its argument\n");
+			return false;
+		} catch (std::out_of_range e) {
+			printf("argument %S out of range\n");
+			return false;
+		}
 	});
 }
 
-void ChatCommands::PconCmd(const vector<wstring> &args) {
+void ChatCommands::AddCommand(wstring cmd, Handler_t handler) {
+	if (commands_.find(cmd) != commands_.end()) {
+		printf("[WARNING] Adding command %S which is already in the map!\n", cmd.c_str());
+	}
+	commands_[cmd] = handler;
+	GWCA::Api().Chat().RegisterCommand(cmd, std::bind(&ChatCommands::ParseCommand,
+		this, std::placeholders::_1, std::placeholders::_2));
+}
 
+void ChatCommands::ParseCommand(wstring cmd, vector<wstring> args) {
+	if (commands_.find(cmd) != commands_.end()) {
+		bool ret = commands_[cmd](args);
+	}
+}
+
+bool ChatCommands::PconCmd(vector<wstring> args) {
 	PconPanel& pcons = GWToolbox::instance().main_window().pcon_panel();
 	if (args.size() == 0) {
 		pcons.ToggleActive();
-	}
-
-	if (args.size() == 1) {
+	} else if (args.size() == 1) {
 		if (args[0].compare(L"on") == 0) {
 			pcons.SetActive(true);
 		} else if (args[0].compare(L"off") == 0) {
 			pcons.SetActive(false);
+		} else {
+			return false;
 		}
 	}
+	return true;
 }
--- GWToolbox/GWToolbox/ChatCommands.h
+++ GWToolbox/GWToolbox/ChatCommands.h
@@ -2,11 +2,25 @@
 
 #include <string>
 #include <vector>
+#include <map>
+#include <functional>
 
 class ChatCommands {
+	typedef std::function<bool(std::vector<std::wstring>)> Handler_t;
+
 public:
 	ChatCommands();
 
 private:
-	void PconCmd(const std::vector<std::wstring> &args);
+	void AddCommand(std::wstring cmd, Handler_t);
+
+	void ParseCommand(std::wstring cmd, std::vector<std::wstring> args);
+
+	bool PconCmd(std::vector<std::wstring> args);
+
+	// map from command to handler executing the command
+	// the handler takes a vector of arguments
+	// returns true if successful, false if error. 
+	// On error, a sequence of commands will stop executing
+	std::map<std::wstring, Handler_t> commands_;
 };
