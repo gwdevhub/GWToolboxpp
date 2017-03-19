fd3abededcc44d6013ecaf70bf7d0b57e719860d
HasKha <ggori@live.com>
2017-03-18 18:11:22 -0700

    bond monitor now supports heroes, henchmen and allies

--- GWToolbox/GWToolbox/Windows/BondsWindow.cpp
+++ GWToolbox/GWToolbox/Windows/BondsWindow.cpp
@@ -15,7 +15,7 @@
 #include "OtherModules\ToolboxSettings.h"
 #include <OtherModules\Resources.h>
 
-DWORD BondsWindow::buff_id[MAX_PLAYERS][MAX_BONDS] = { 0 };
+DWORD BondsWindow::buff_id[MAX_PARTYSIZE][MAX_BONDS] = { 0 };
 
 void BondsWindow::Initialize() {
 	ToolboxWidget::Initialize();
@@ -49,34 +49,36 @@ void BondsWindow::Terminate() {
 void BondsWindow::Draw(IDirect3DDevice9* device) {
 	if (!visible) return;
 
-	static bool update_build = true;
 	switch (GW::Map().GetInstanceType()) {
 	case GW::Constants::InstanceType::Explorable:
-		if (update_build) {
-			if (UpdateSkillbarBonds()) update_build = false;
+		if (update) {
+			update = false;
+			if (!UpdateSkillbarBonds()) update = true;
+			if (!UpdatePartyIndexMap()) update = true;
 		}
 		break;
 	case GW::Constants::InstanceType::Outpost:
-		update_build = true;
+		update = true;
 		UpdateSkillbarBonds();
 		break;
 	case GW::Constants::InstanceType::Loading:
-		update_build = true;
-		return;
-		break;
+		update = true;
+		return; // exit here instead
 	default:
 		break;
 	}
 	
+	GW::PartyInfo* info = GW::Partymgr().GetPartyInfo();
+	if (info == nullptr) return;
+	GW::gw_array<GW::AgentID>& allies = info->others;
+	
 	int img_size = row_height > 0 ? row_height : GuiUtils::GetPartyHealthbarHeight();
-	int party_size = GW::Partymgr().GetPartySize();
+	unsigned int party = agentids.size();
+	unsigned int full_party = party + (show_allies ? allies.size() : 0);
 
-	int size = GW::Partymgr().GetPartySize();
-	if (size > MAX_PLAYERS) size = MAX_PLAYERS;
-
-	for (int i = 0; i < MAX_PLAYERS; ++i) {
-		for (int j = 0; j < n_bonds; ++j) {
-			buff_id[i][j] = 0;
+	for (PartyIndex p = 0; p < full_party && p < MAX_PARTYSIZE; ++p) {
+		for (BondIndex b = 0; b < n_bonds; ++b) {
+			buff_id[p][b] = 0;
 		}
 	}
 
@@ -87,40 +89,52 @@ void BondsWindow::Draw(IDirect3DDevice9* device) {
 
 		if (buffs.valid() && agents.valid() && effects.valid()) {
 			for (size_t i = 0; i < buffs.size(); ++i) {
-				int player = -1;
-				DWORD target_id = buffs[i].TargetAgentId;
-				if (target_id < agents.size() && agents[target_id]) {
-					player = agents[target_id]->PlayerNumber;
-					if (player == 0 || player > MAX_PLAYERS) continue;
-					--player;	// player numbers are from 1 to partysize in party list
-				}
-				if (player == -1) continue;
-
-				int bond = -1;
-				for (int j = 0; j < n_bonds; ++j) {
-					if (buffs[i].SkillId == skillbar_bond_skillid[j]) {
-						bond = j;
+				PartyIndex p = -1;
+				DWORD id = buffs[i].TargetAgentId;
+				if (party_index.find(id) != party_index.end()) {
+					p = party_index[id];
+				} else if (show_allies) {
+					// check allies
+					for (unsigned int j = 0; j < allies.size(); ++j) {
+						if (allies[j] == id) {
+							p = party + j;
+						}
 					}
 				}
-				if (bond == -1) continue;
+				if (p == -1) continue;
+				if (p >= MAX_PARTYSIZE) continue;
 
-				buff_id[player][bond] = buffs[i].BuffId;
+				BondIndex b = -1;
+				for (BondIndex b_idx = 0; b_idx < n_bonds; ++b_idx) {
+					if (buffs[i].SkillId == skillbar_bond_skillid[b_idx]) {
+						b = b_idx;
+						break;
+					}
+				}
+				if (b == -1) continue;
+
+				buff_id[p][b] = buffs[i].BuffId;
 			}
 		}
 	}
 	ImGui::PushStyleVar(ImGuiStyleVar_WindowRounding, 0.0f);
 	ImGui::PushStyleVar(ImGuiStyleVar_WindowPadding, ImVec2(0.0f, 0.0f));
 	ImGui::PushStyleColor(ImGuiCol_WindowBg, ImColor(background));
-	ImGui::SetNextWindowSize(ImVec2((float)(n_bonds * img_size), (float)(party_size * img_size)));
+	ImGui::SetNextWindowSize(ImVec2((float)(n_bonds * img_size), 
+		(float)((full_party + (full_party > party ? 1 : 0)) * img_size)));
 	if (ImGui::Begin(Name(), &visible, GetWinFlags(0, !click_to_use))) {
 		float x = ImGui::GetWindowPos().x;
 		float y = ImGui::GetWindowPos().y;
-		for (int player = 0; player < party_size; ++player) {
-			for (int bond = 0; bond < n_bonds; ++bond) {
-				ImVec2 tl(x + (bond + 0) * img_size, y + (player + 0) * img_size);
-				ImVec2 br(x + (bond + 1) * img_size, y + (player + 1) * img_size);
-				if (buff_id[player][bond] > 0) {
-					ImGui::GetWindowDrawList()->AddImage((ImTextureID)textures[skillbar_bond_idx[bond]],
+		for (PartyIndex p = 0; p < full_party && p < MAX_PARTYSIZE; ++p) {
+			int extra = p >= party ? 1 : 0;
+			float ytop = y + (p + 0 + extra) * img_size;
+			float ybot = y + (p + 1 + extra) * img_size;
+			for (BondIndex b = 0; b < n_bonds; ++b) {
+				ImVec2 tl(x + (b + 0) * img_size, ytop);
+				ImVec2 br(x + (b + 1) * img_size, ybot);
+				if (buff_id[p][b] > 0) {
+					ImGui::GetWindowDrawList()->AddImage(
+						(ImTextureID)textures[skillbar_bond_idx[b]],
 						ImVec2(tl.x, tl.y),
 						ImVec2(br.x, br.y));
 				}
@@ -128,10 +142,24 @@ void BondsWindow::Draw(IDirect3DDevice9* device) {
 					if (ImGui::IsMouseHoveringRect(tl, br)) {
 						ImGui::GetWindowDrawList()->AddRect(tl, br, IM_COL32(255, 255, 255, 255));
 						if (ImGui::IsMouseReleased(0)) {
-							if (buff_id[player][bond] > 0) {
-								GW::Effects().DropBuff(buff_id[player][bond]);
+							if (buff_id[p][b] > 0) {
+								GW::Effects().DropBuff(buff_id[p][b]);
 							} else {
-								UseBuff(player, bond);
+								GW::AgentID target;
+								if (p < agentids.size()) {
+									target = agentids[p];
+								} else if (p - party >= 0 && p - party < allies.size()) {
+									target = allies[p - party];
+								}
+								if (target > 0) {
+									int slot = skillbar_bond_slot[b];
+									GW::Skillbar skillbar = GW::Skillbar::GetPlayerSkillbar();
+									if (skillbar.IsValid() && skillbar.Skills[slot].Recharge == 0) {
+										GW::Gamethread().Enqueue([slot, target] {
+											GW::Skillbarmgr().UseSkill(slot, target);
+										});
+									}
+								}
 							}
 						}
 					}
@@ -144,10 +172,41 @@ void BondsWindow::Draw(IDirect3DDevice9* device) {
 	ImGui::PopStyleVar(2);
 }
 
-void BondsWindow::UseBuff(int player, int bond) {
+bool BondsWindow::UpdatePartyIndexMap() {
+	party_index.clear();
+	agentids.clear();
+
+	GW::PartyInfo* info = GW::Partymgr().GetPartyInfo();
+	if (info == nullptr) return false;
+
+	GW::PlayerArray players = GW::Agents().GetPlayerArray();
+	if (!players.valid()) return false;
+
+	int index = 0;
+	for (GW::PlayerPartyMember& player : info->players) {
+		long id = players[player.loginnumber].AgentID;
+		party_index[id] = index++;
+		agentids.push_back(id);
+
+		for (GW::HeroPartyMember& hero : info->heroes) {
+			if (hero.ownerplayerid == player.loginnumber) {
+				party_index[hero.agentid] = index++;
+				agentids.push_back(hero.agentid);
+			}
+		}
+	}
+	for (GW::HenchmanPartyMember& hench : info->henchmen) {
+		party_index[hench.agentid] = index++;
+		agentids.push_back(hench.agentid);
+	}
+
+	return true;
+}
+
+void BondsWindow::UseBuff(PartyIndex player, BondIndex bond) {
 	if (GW::Map().GetInstanceType() != GW::Constants::InstanceType::Explorable) return;
 
-	DWORD target = GW::Agents().GetAgentIdByLoginNumber(player + 1);
+	DWORD target = agentids[player];
 	if (target == 0) return;
 
 	int slot = skillbar_bond_slot[bond];
@@ -164,6 +223,7 @@ void BondsWindow::LoadSettings(CSimpleIni* ini) {
 	ToolboxWidget::LoadSettings(ini);
 	background = Colors::Load(ini, Name(), "background", Colors::ARGB(76, 0, 0, 0));
 	click_to_use = ini->GetBoolValue(Name(), "click_to_use", true);
+	show_allies = ini->GetBoolValue(Name(), "show_allies", true);
 	row_height = ini->GetLongValue(Name(), "row_height", 0);
 }
 
@@ -171,14 +231,20 @@ void BondsWindow::SaveSettings(CSimpleIni* ini) {
 	ToolboxWidget::SaveSettings(ini);
 	Colors::Save(ini, Name(), "background", background);
 	ini->SetBoolValue(Name(), "click_to_use", click_to_use);
+	ini->SetBoolValue(Name(), "show_allies", show_allies);
 	ini->SetLongValue(Name(), "row_height", row_height);
 }
 
 void BondsWindow::DrawSettingInternal() {
 	Colors::DrawSetting("Background", &background);
 	ImGui::Checkbox("Click to Drop/Use", &click_to_use);
+	ImGui::Checkbox("Show bonds for Allies", &show_allies);
 	ImGui::InputInt("Row Height", &row_height);
+	if (row_height < 0) row_height = 0;
 	ImGui::ShowHelp("Height of each row, leave 0 for default");
+	if (ImGui::SmallButton("Update bond order")) update = true;
+	ImGui::ShowHelp("You can use this button after reordering your build in an\n"
+					"explorable area to force a refresh in the bond order.");
 }
 
 bool BondsWindow::UpdateSkillbarBonds() {
--- GWToolbox/GWToolbox/Windows/BondsWindow.h
+++ GWToolbox/GWToolbox/Windows/BondsWindow.h
@@ -2,6 +2,7 @@
 
 #include <vector>
 
+#include <GWCA\GameEntities\Agent.h>
 #include <GWCA\Constants\Skills.h>
 
 #include "Color.h"
@@ -27,7 +28,7 @@ class BondsWindow : public ToolboxWidget {
 		WatchfulSpirit,
 	};
 
-	static const int MAX_PLAYERS = 12;
+	static const int MAX_PARTYSIZE = 64;
 
 	BondsWindow() {};
 	~BondsWindow() {};
@@ -53,20 +54,33 @@ public:
 	void DrawSettingInternal() override;
 
 private:
-	void UseBuff(int player, int bond);
+	typedef unsigned int PartyIndex;
+	typedef unsigned int BondIndex;
+	typedef DWORD BuffID;
+
+	void UseBuff(PartyIndex player, BondIndex bond);
 	GW::Constants::SkillID GetSkillID(Bond bond) const;
 	bool UpdateSkillbarBonds();
+	bool UpdatePartyIndexMap();
 
 	IDirect3DTexture9* textures[MAX_BONDS];
 	Color background;
 
+	// settings
 	bool click_to_use = true;
+	bool show_allies = true;
 	int row_height = 0;
 
-	static DWORD buff_id[MAX_PLAYERS][MAX_BONDS];
+	static BuffID buff_id[MAX_PARTYSIZE][MAX_BONDS];
 
-	int n_bonds;
-	std::vector<int> skillbar_bond_idx;
-	std::vector<int> skillbar_bond_slot;
-	std::vector<DWORD> skillbar_bond_skillid;
+	bool update = true;
+
+	unsigned int n_bonds;
+	std::vector<BondIndex> skillbar_bond_idx;
+	std::vector<BondIndex> skillbar_bond_slot;
+	std::vector<BuffID> skillbar_bond_skillid;
+	
+	// map from agent id to index
+	std::map<GW::AgentID, PartyIndex> party_index;
+	std::vector<GW::AgentID> agentids;
 };
