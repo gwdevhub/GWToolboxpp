acbb5ab1eaaa15fe31be382ff780a2bc9ad053f4
reduf <laurent.dufresne@hotmail.com>
2017-03-24 11:48:02 -0400

    add two messages to ChatFilter & updated GWCA.

--- GWToolbox/GWToolbox/OtherModules/ChatFilter.cpp
+++ GWToolbox/GWToolbox/OtherModules/ChatFilter.cpp
@@ -328,6 +328,9 @@ bool ChatFilter::ShouldIgnore(const wchar_t *message) {
 	case 0x880: return false; // 'Player name <name> is invalid.'. (Anyone actually saw it ig ?)
 	case 0x881: return false; // 'Player <name> is not online.' (Offline)
 
+	case 0x7BF4: return false; // You have been playing for x time.
+	case 0x7BF5: return false; // You have been playinf for x time. Please take a break.
+
 	case 0x8101:
 		switch (message[1]) {
 		case 0x1868: // teilah takes 10 festival tickets
@@ -353,6 +356,7 @@ bool ChatFilter::ShouldIgnore(const wchar_t *message) {
 		case 0x4651: return pvp_messages; // a hero skill has been updated for pvp
 		case 0x223B: return hoh; // a party won hall of heroes	
 		case 0x23E4: return favor; // 0xF8AA 0x95CD 0x2766 // the world no longer has the favor of the gods
+		case 0x2E36: return false; // 0xEA8C 0xE05B 0x330F // Player has achieved the title...
 		case 0x3772: return false; // I'm under the effect of x
 		}
 		break;
