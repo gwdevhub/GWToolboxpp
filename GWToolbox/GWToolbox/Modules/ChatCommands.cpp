8b590f90e96b5eb6c4ef280bea60e401408399ae
reduf <laurent.dufresne@hotmail.com>
2018-02-14 11:09:46 -0500

    fix arrows who didn't get capture when camera was unlocked

--- GWToolbox/GWToolbox/Modules/ChatCommands.cpp
+++ GWToolbox/GWToolbox/Modules/ChatCommands.cpp
@@ -102,20 +102,20 @@ void ChatCommands::Initialize() {
 	GW::Chat::CreateCommand(L"transmo", ChatCommands::CmdTransmo);
 }
 
-#define KEY_ESC 0x1B
-#define KEY_A 0x41
-#define KEY_D 0x44
-#define KEY_E 0x45
-#define KEY_Q 0x51
-#define KEY_R 0x52
-#define KEY_S 0x53
-#define KEY_W 0x57
-#define KEY_X 0x58
-#define KEY_Z 0x5A
-#define KEY_LEFT 0x25
-#define KEY_UP 0x26
-#define KEY_RIGHT 0x27
-#define KEY_DOWN 0x28
+#define KEY_ESC		0x1B
+#define KEY_LEFT	0x25
+#define KEY_UP		0x26
+#define KEY_RIGHT	0x27
+#define KEY_DOWN	0x28
+#define KEY_A		0x41
+#define KEY_D		0x44
+#define KEY_E		0x45
+#define KEY_Q		0x51
+#define KEY_R		0x52
+#define KEY_S		0x53
+#define KEY_W		0x57
+#define KEY_X		0x58
+#define KEY_Z		0x5A
 
 bool ChatCommands::WndProc(UINT Message, WPARAM wParam, LPARAM lParam) {
 	if (!GW::CameraMgr::GetCameraUnlock()) return false;
@@ -135,6 +135,12 @@ bool ChatCommands::WndProc(UINT Message, WPARAM wParam, LPARAM lParam) {
 				case KEY_W:
 				case KEY_X:
 				case KEY_Z:
+				
+				case KEY_ESC:
+				case KEY_UP:
+				case KEY_DOWN:
+				case KEY_LEFT:
+				case KEY_RIGHT:
 					return true;
 			}
 	}
