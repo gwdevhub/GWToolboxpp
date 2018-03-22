47ca8606eba8a40f5dcc04ab3d749cde11dbac1a
HasKha <ggori@live.com>
2018-03-21 22:15:24 -0700

    fixed compile issue in Release mode

--- GWToolbox/GWToolbox/logger.cpp
+++ GWToolbox/GWToolbox/logger.cpp
@@ -29,7 +29,7 @@ void Log::InitializeLog() {
 	freopen_s(&fh, "CONOUT$", "w", stderr);
 	SetConsoleTitle("GWTB++ Debug Console");
 #else
-	logfile = freopen(Resources::GetPath("log.txt").c_str(), "w", stdout);
+	logfile = _wfreopen(Resources::GetPath(L"log.txt").c_str(), L"w", stdout);
 #endif
 }
 
