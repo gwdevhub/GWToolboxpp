78f97d34732fd837ef7e59fb78c5b24e01a557e3
Jon Riley <jon@3vcloud.uk>
2019-02-05 12:17:42 +0000

    Set pcons to automatically disable when objectives complete in fow and uw, and within range of urgoz/kanaxai

--- GWToolbox/GWToolbox/Windows/PconsWindow.h
+++ GWToolbox/GWToolbox/Windows/PconsWindow.h
@@ -5,6 +5,8 @@
 #include <Defines.h>
 
 #include <GWCA\Constants\Constants.h>
+#include <GWCA\GameEntities\Position.h>
+#include <GWCA\GameEntities\Agent.h>
 
 #include "Timer.h"
 #include "Pcons.h"
@@ -24,6 +26,7 @@ public:
 	void Initialize() override;
 
 	bool SetEnabled(bool b);
+
 	inline void ToggleEnable() { SetEnabled(!enabled); }
 
 	void Update(float delta) override;
@@ -38,19 +41,48 @@ public:
 private:
 	void CheckIfWeJustEnabledAlcoholWithLunarsOn();
 
+
 	std::vector<Pcon*> pcons;
 	PconAlcohol* pcon_alcohol = nullptr;
 	GW::Constants::InstanceType current_map_type;
 	clock_t scan_inventory_timer;
 	bool enabled = false;
-	
+
 	// Interface Settings
 	bool tick_with_pcons = false;
 	int items_per_row = 3;
 	bool show_enable_button = true;
+	GW::Agent* player;
 
 	// Pcon Settings
 	// todo: tonic pop?
 	// todo: morale / dp removal
 	// todo: replenish character pcons from chest?
+
+	// Elite area auto disable
+	void CheckBossRangeAutoDisable();	// Trigger Elite area auto disable if applicable
+	void MapChanged();
+	void CheckObjectivesCompleteAutoDisable();
+
+	GW::Constants::MapID map_id;
+	bool elite_area_disable_triggered = false;	// Already triggered in this run?
+	clock_t elite_area_check_timer;
+
+	std::vector<DWORD> objectives_complete;
+	// Fissue of Woe
+	bool fow_disable_when_all_objs_complete = false;
+	std::vector<DWORD> fow_objectives{ 309,310,311,312,313,314,315,316,317,318,319 };
+	char* fow_disable_hint = "Fissure of Woe: Pcons auto disabled on final objective completion";
+	// Underworld
+	bool uw_disable_when_all_objs_complete = false;
+	std::vector<DWORD> uw_objectives{ 157 };
+	char* uw_disable_hint = "The Underworld: Pcons auto disabled on final objective completion";
+	// Urgoz Warren
+	GW::Vector2f urgoz_location;
+	bool urgoz_disable_in_range_of_boss = false;	// Urgoz
+	char* urgoz_disable_hint = "Urgoz Warren: Pcons auto disabled after entering final room";
+	// The Deep
+	GW::Vector2f kanaxai_location;
+	bool deep_disable_in_range_of_boss = false;	// Deep
+	char* deep_disable_hint = "The Deep: Pcons auto disabled after entering final room";
 };
