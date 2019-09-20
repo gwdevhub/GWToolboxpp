75ffb7f36348857d96ec2ea413a59b5e8902d74e
Jon Riley <jon@3vcloud.uk>
2019-09-20 23:37:18 +0100

    Version number updated

--- GWToolbox/GWToolbox/Defines.h
+++ GWToolbox/GWToolbox/Defines.h
@@ -1,7 +1,7 @@
 #pragma once
 
 #define GWTOOLBOX_VERSION "2.26" // Enter main version here. Should match with server version file.
-#define GWTOOLBOX_EXTENDED_VERSION "1.5 (discord)" // Extended version
+#define GWTOOLBOX_EXTENDED_VERSION "1.6 (discord)" // Extended version
 #define BETA_VERSION "" // enter something like "Beta 1". Internally we only check if not empty
 #define GWTOOLBOX_WEBSITE "https://haskha.github.io/GWToolboxpp/"
 #define GWTOOLBOX_EXTENDED_WEBSITE "https://github.com/3vcloud/GWToolboxpp"
