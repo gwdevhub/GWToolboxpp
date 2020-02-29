52738e0e0c766ff582515ddbdc1932b372e0ddff
HasKha <>
2020-02-29 11:19:28 -0800

    added cmake project generation

---
+++ cmake/easywsclient.cmake
@@ -0,0 +1,15 @@
+include_guard()
+
+set(EASYWSCLIENT_FOLDER "${PROJECT_SOURCE_DIR}/Dependencies/easywsclient/")
+
+set(SOURCES 
+	"${EASYWSCLIENT_FOLDER}/easywsclient.hpp"
+	"${EASYWSCLIENT_FOLDER}/easywsclient.cpp")
+
+add_library(easywsclient)
+target_sources(easywsclient PRIVATE ${SOURCES})
+target_include_directories(easywsclient PUBLIC "${EASYWSCLIENT_FOLDER}")
+
+include(openssl)
+
+target_link_libraries(easywsclient PUBLIC openssl)
---
+++ cmake/simpleini.cmake
@@ -0,0 +1,5 @@
+include_guard()
+
+add_library(simpleini INTERFACE)
+target_include_directories(simpleini INTERFACE "${PROJECT_SOURCE_DIR}/dependencies/")
+
