a8504a5af8d00a472bb9b7d3d1c591d10b56c024
Laurent Dufresne <laurent.dufresne@hotmail.com>
2020-05-02 21:20:18 +0200

    Fix bug with updater not saving version

--- GWToolbox/GWToolbox/Modules/Updater.h
+++ GWToolbox/GWToolbox/Modules/Updater.h
@@ -3,50 +3,43 @@
 #include "ToolboxUIElement.h"
 
 class Updater : public ToolboxUIElement {
-	Updater() {};
-	~Updater() {};
+    Updater() {};
+    ~Updater() {};
 public:
-	static Updater& Instance() {
-		static Updater instance;
-		return instance;
-	}
-    struct GWToolboxRelease {
-        std::string body;
-        std::string version;
-        std::string download_url;
-    };
-	const char* Name() const override { return "Updater"; }
+    static Updater& Instance() {
+        static Updater instance;
+        return instance;
+    }
 
-	void CheckForUpdate(const bool forced = false);
-	void DoUpdate();
+    const char* Name() const override { return "Updater"; }
 
-	void Draw(IDirect3DDevice9* device) override;
+    void CheckForUpdate();
+    void DoUpdate();
 
-	void LoadSettings(CSimpleIni* ini) override;
-	void SaveSettings(CSimpleIni* ini) override;
-	void Initialize() override;
-	void DrawSettings() override {};
-	void DrawSettingInternal() override;
-    void GetLatestRelease(GWToolboxRelease*);
-	std::string GetServerVersion() const { return latest_release.version; }
+    void Draw(IDirect3DDevice9* device) override;
+
+    void LoadSettings(CSimpleIni* ini) override;
+    void SaveSettings(CSimpleIni* ini) override;
+    void Initialize() override;
+    void DrawSettings() override {};
+    void DrawSettingInternal() override;
+
+    std::string GetServerVersion() const { return server_version; }
 
 private:
-    GWToolboxRelease latest_release;
+    std::string server_version = "";
+    // 0=none, 1=check and warn, 2=check and ask, 3=check and do
+    int mode = 0;
 
-	// 0=none, 1=check and warn, 2=check and ask, 3=check and do
-	int mode = 2;
+    // 0=checking, 1=asking, 2=downloading, 3=done
+    enum Step {
+        Checking,
+        Asking,
+        Downloading,
+        Success,
+        Done
+    };
 
-	// 0=checking, 1=asking, 2=downloading, 3=done
-	enum Step {
-		Checking,
-		Asking,
-		Downloading,
-		Success,
-		Done
-	};
-	bool notified = false;
-	bool forced_ask = false;
-	clock_t last_check = 0;
-
-	Step step = Checking;
+    Step step = Checking;
+    std::string changelog = "";
 };
