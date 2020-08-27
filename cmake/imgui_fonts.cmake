179e883d3bbf929d98d0a144c54e741838ec37ce
HasKha <>
2020-08-27 12:27:43 -0700

    fixed url

--- cmake/imgui_fonts.cmake
+++ cmake/imgui_fonts.cmake
@@ -5,6 +5,6 @@ endif()
 include(FetchContent)
 FetchContent_Declare(
     imgui_fonts
-    GIT_REPOSITORY git@github.com:HasKha/imgui-fonts.git
+    GIT_REPOSITORY https://github.com/HasKha/imgui-fonts.git
     GIT_TAG 4b785f4871b651d1d35be6286726d74c5804f04e)
 FetchContent_MakeAvailable(imgui_fonts)
