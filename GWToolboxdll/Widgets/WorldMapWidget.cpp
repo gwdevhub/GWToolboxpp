26bbca20aa33413d066310af645d4e1ad41d3156
Jon <>
2021-07-03 08:25:06 +0100

    Revert "Don't show hard mode tick on world map unless its unlocked"
    This reverts commit 85e843e3410226958796cc26fc76534585110fd3.

--- GWToolboxdll/Widgets/WorldMapWidget.cpp
+++ GWToolboxdll/Widgets/WorldMapWidget.cpp
@@ -29,39 +29,6 @@ namespace {
     bool showing_all_outposts = false;
 
     bool drawn = false;
-
-    bool IsHardModeUnlocked() {
-        const GW::GameContext* g = GW::GameContext::instance();
-        const GW::WorldContext* w = g ? g->world : nullptr;
-        if (!w) return false;
-        const GW::AreaInfo* map = GW::Map::GetCurrentMapInfo();
-        if (!map) return false;
-        auto ArrayBoolAt = [](const GW::Array<uint32_t>&array, uint32_t index) {
-            uint32_t real_index = index / 32;
-            if (!array.valid() || real_index >= array.size())
-                return false;
-            uint32_t shift = index % 32;
-            uint32_t flag = 1 << shift;
-            return (array[real_index] & flag) != 0;
-        };
-        uint32_t map_id = 0;
-        switch (map->campaign) {
-        case 0:
-            map_id = static_cast<uint32_t>(GW::Constants::MapID::Hells_Precipice);
-            break;
-        case 1:
-            map_id = static_cast<uint32_t>(GW::Constants::MapID::Imperial_Sanctum_outpost_mission);
-            break;
-        case 2:
-            map_id = static_cast<uint32_t>(GW::Constants::MapID::Abaddons_Gate);
-            break;
-        case 3:
-        case 4:
-            map_id = static_cast<uint32_t>(GW::Constants::MapID::A_Time_for_Heroes_mission);
-            break;
-        }
-        return map_id && ArrayBoolAt(w->missions_completed, map_id);
-    }
 }
 
 WorldMapWidget::WorldMapWidget() {
@@ -156,11 +123,9 @@ void WorldMapWidget::Draw(IDirect3DDevice9 *pDevice) {
         UNREFERENCED_PARAMETER(world);
         ImGui::Checkbox("Show all areas", &showing_all_outposts);
         show_all_rect = ImGui::GetCurrentWindow()->DC.LastItemRect;
-        if (IsHardModeUnlocked()) {
-            bool is_hard_mode = GW::PartyMgr::GetIsPartyInHardMode();
-            ImGui::Checkbox("Hard mode", &is_hard_mode);
-            hard_mode_rect = ImGui::GetCurrentWindow()->DC.LastItemRect;
-        }
+        bool is_hard_mode = GW::PartyMgr::GetIsPartyInHardMode();
+        ImGui::Checkbox("Hard mode", &is_hard_mode);
+        hard_mode_rect = ImGui::GetCurrentWindow()->DC.LastItemRect;
     }
     ImGui::End();
     ImGui::PopStyleColor();
