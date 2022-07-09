gwdevhub/GWToolboxpp

commit 4477008279918308cd9ba9d3a74c09ec1bfbc15f

Fixed UI bugs with effect monitor and bond monitor widgets (HR/hex)

GWToolboxdll/Widgets/SkillbarWidget.cpp
@@ -40,7 +40,7 @@ std::vector<SkillbarWidget::Effect> SkillbarWidget::get_effects(const uint32_t s
     auto* effects = GW::Effects::GetPlayerEffects();
     if (!effects) return ret;
     for (const GW::Effect& effect : *effects) {
-        if (effect.skill_id == skillId && effect.effect_type != 7 /*hex*/) {
+        if (effect.skill_id == skillId) {
             Effect e;
             e.remaining = effect.GetTimeRemaining();
             if (effect.duration) {
@@ -60,7 +60,7 @@ SkillbarWidget::Effect SkillbarWidget::get_longest_effect(const uint32_t skillId
     auto* effects = GW::Effects::GetPlayerEffects();
     if (!effects) return ret;
     for (const GW::Effect& effect : *effects) {
-        if (effect.skill_id == skillId && effect.effect_type != 7 /*hex*/) {
+        if (effect.skill_id == skillId) {
             const auto remaining = effect.GetTimeRemaining();
             if (ret.remaining < remaining) {
                 ret.remaining = remaining;

GWToolboxdll/Widgets/BondsWidget.cpp
@@ -154,16 +154,10 @@ void BondsWidget::Draw(IDirect3DDevice9* device) {
                     bool overlay = false;
                     const GW::Skill* skill_data = GW::SkillbarMgr::GetSkillConstantData((uint32_t) skill_id);
                     if (!skill_data || skill_data->duration0 == 0x20000) continue; // Maintained skill/enchantment
-                    const GW::Attribute* attribute;
                     const GW::Attribute* agentAttributes = GW::PartyMgr::GetAgentAttributes(agent_id);
-                    if (!agentAttributes) { // nullptr for heros as they have agent_id 0 in world->attributes
-                        const GW::PartyAttribute& partyAttribute = GW::GameContext::instance()->world->attributes[0];
-                        attribute = &partyAttribute.attribute[skill_data->attribute];
-                    } else {
-                        attribute = &agentAttributes[skill_data->attribute];
-                    }
-                    if (!attribute) continue;
-                    if (effect.effect_type < attribute->level) overlay = true;
+                    assert(agentAttributes);
+                    agentAttributes = &agentAttributes[skill_data->attribute];
+                    overlay = effect.attribute_level < agentAttributes->level;
 
                     size_t y = party_map[agent_id];
                     size_t x = bond_map[skill_id];

GWToolboxdll/Widgets/EffectsMonitorWidget.cpp
@@ -35,7 +35,7 @@ void EffectsMonitorWidget::RefreshEffects() {
     struct Packet : GW::Packet::StoC::PacketBase {
         uint32_t agent_id;
         uint32_t skill_id;
-        uint32_t effect_type;
+        uint32_t attribute_level;
         uint32_t effect_id;
         float duration;
     } add;
@@ -53,7 +53,7 @@ void EffectsMonitorWidget::RefreshEffects() {
         add.skill_id = effect.skill_id;
         add.effect_id = effect.effect_id;
         add.duration = effect.duration;
-        add.effect_type = effect.effect_type;
+        add.attribute_level = effect.attribute_level;
         GW::StoC::EmulatePacket(&add);
     }
     Instance().SetMoralePercent(GW::GameContext::instance()->world->morale);

GWToolboxdll/Windows/InfoWindow.cpp
@@ -301,7 +301,7 @@ void InfoWindow::DrawAgentInfo(GW::Agent* agent) {
         GW::EffectArray* effects = GW::Effects::GetAgentEffects(agent->agent_id);
         if (effects) {
             for (auto& effect : *effects) {
-                ImGui::Text("id: %d | type: %d | skill: %d | duration: %u", effect.effect_id, effect.effect_type, effect.skill_id, effect.GetTimeRemaining() / 1000);
+                ImGui::Text("id: %d | attrib level: %d | skill: %d | duration: %u", effect.effect_id, effect.attribute_level, effect.skill_id, effect.GetTimeRemaining() / 1000);
             }
         }
         ImGui::TreePop();

commit febaa9a5a1b05e26b6be718dab7ee1f9b32ee4ab

Regression: skillbar widget shows hex effect durations
caused by:
https://github.com/HasKha/GWToolboxpp/commit/4477008279918308cd9ba9d3a74c09ec1bfbc15f

GWToolboxdll/Widgets/SkillbarWidget.cpp
@@ -39,6 +39,8 @@ std::vector<SkillbarWidget::Effect> SkillbarWidget::get_effects(const uint32_t s
     std::vector<Effect> ret;
     auto* effects = GW::Effects::GetPlayerEffects();
     if (!effects) return ret;
+    const auto& skill_data = GW::SkillbarMgr::GetSkillConstantData(skillId);
+    if (skill_data && skill_data->type == static_cast<uint32_t>(GW::Constants::SkillType::Hex)) return ret;
     for (const GW::Effect& effect : *effects) {
         if (effect.skill_id == skillId) {
             Effect e;
@@ -59,6 +61,8 @@ SkillbarWidget::Effect SkillbarWidget::get_longest_effect(const uint32_t skillId
     SkillbarWidget::Effect ret;
     auto* effects = GW::Effects::GetPlayerEffects();
     if (!effects) return ret;
+    const auto& skill_data = GW::SkillbarMgr::GetSkillConstantData(skillId);
+    if (skill_data && skill_data->type == static_cast<uint32_t>(GW::Constants::SkillType::Hex)) return ret;
     for (const GW::Effect& effect : *effects) {
         if (effect.skill_id == skillId) {
             const auto remaining = effect.GetTimeRemaining();