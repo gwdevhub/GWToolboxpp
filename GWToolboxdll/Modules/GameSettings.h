766709aff2118ae340759fc7cc2a911733cf96e4
DubbleClick <m@pyc.ac>
2022-08-12 21:28:29 +0200

    block zero exp gained by default

--- GWToolboxdll/Modules/GameSettings.h
+++ GWToolboxdll/Modules/GameSettings.h
@@ -235,7 +235,7 @@ public:
 
     bool block_faction_gain = false;
     bool block_experience_gain = false;
-    bool block_zero_experience_gain = false;
+    bool block_zero_experience_gain = true;
 
     Color nametag_color_npc = NAMETAG_COLOR_DEFAULT_NPC;
     Color nametag_color_player_self = NAMETAG_COLOR_DEFAULT_PLAYER_SELF;
