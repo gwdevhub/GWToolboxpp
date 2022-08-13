43ec82871721f6f093a629df56bebf87611aec59
DubbleClick <m@pyc.ac>
2022-08-13 12:51:26 +0200

    update earmake

--- cmake/earcut.cmake
+++ cmake/earcut.cmake
@@ -4,7 +4,7 @@ include(FetchContent)
 FetchContent_Declare(
     earcut
     GIT_REPOSITORY https://github.com/mapbox/earcut.hpp.git
-    GIT_TAG a3807695eb92d208643e15e112982b482ce36c92)
+    GIT_TAG b28acde132cdb8e0ef536a96ca7ada8a651f9169)
 FetchContent_GetProperties(earcut)
 if (earcut_POPULATED)
     return()
