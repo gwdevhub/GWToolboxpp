f4106d832be701fd2e5a0775c2140b4b241cfd4f
DubbleClick <m@pyc.ac>
2022-08-25 15:57:38 +0200

    don't set capture in imgui wndproc (fix mouse movement issues?)

--- GWToolboxdll/imgui_impl_win32.cpp
+++ GWToolboxdll/imgui_impl_win32.cpp
@@ -537,7 +537,7 @@ IMGUI_IMPL_API LRESULT ImGui_ImplWin32_WndProcHandler(HWND hwnd, UINT msg, WPARA
             if (msg == WM_XBUTTONDOWN || msg == WM_XBUTTONDBLCLK) {
                 button = (GET_XBUTTON_WPARAM(wParam) == XBUTTON1) ? 3 : 4;
             }
-            if (bd->MouseButtonsDown == 0 && ::GetCapture() == NULL) ::SetCapture(hwnd);
+            //if (bd->MouseButtonsDown == 0 && ::GetCapture() == NULL) ::SetCapture(hwnd);
             bd->MouseButtonsDown |= 1 << button;
             io.AddMouseButtonEvent(button, true);
             return 0;
