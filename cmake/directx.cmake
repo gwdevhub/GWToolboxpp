8deccb76df0a4f0320ee76bf509654aa0f106cbf
DubbleClick <m@pyc.ac>
2022-09-12 15:01:26 +0200

    remove d3dx dependency

--- cmake/directx.cmake
+++
@@ -1,9 +0,0 @@
-include_guard()
-
-set(DIRECTX_FOLDER "${PROJECT_SOURCE_DIR}/Dependencies/DirectX/build/native/")
-
-add_library(directx INTERFACE)
-target_include_directories(directx INTERFACE "${DIRECTX_FOLDER}/include")
-target_link_libraries(directx INTERFACE
-    "${DIRECTX_FOLDER}/release/lib/x86/d3dx9.lib")
-
