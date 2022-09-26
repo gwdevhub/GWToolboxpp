e9d2a5133af44d39e686f101d57eb2e6be11c72f
DubbleClick <m@pyc.ac>
2022-09-27 01:58:12 +0200

    gwca for plugins

--- modules/clock/clock.cpp
+++ modules/clock/clock.cpp
@@ -1,5 +1,9 @@
 #include "clock.h"
 
+#include "GWCA/Managers/ChatMgr.h"
+#include "GWCA/Managers/GameThreadMgr.h"
+
+#include <format>
 #include <imgui.h>
 
 DLLAPI TBModule* TBModuleInstance()
@@ -13,4 +17,28 @@ void Clock::Draw(IDirect3DDevice9* device)
     ImGui::Begin("clock test");
     ImGui::Text("hello, I am a plugin!");
     ImGui::End();
+}
+
+void CmdClock(const wchar_t*, int, wchar_t**)
+{
+    const auto now = std::chrono::system_clock::now();
+    std::time_t time = std::chrono::system_clock::to_time_t(now);
+    const auto print_time = std::ctime(&time);
+    auto str = std::format("Current time is {}", print_time);
+    str.pop_back();
+    GW::GameThread::Enqueue([str] {
+        GW::Chat::SendChat('#', str.c_str());
+    });
+}
+
+void Clock::Initialize(ImGuiContext* ctx)
+{
+    TBModule::Initialize(ctx);
+    GW::Chat::CreateCommand(L"clock", CmdClock);
+}
+
+void Clock::Terminate()
+{
+    TBModule::Terminate();
+    GW::Chat::DeleteCommand(L"clock");
 }
--- modules/clock/clock.h
+++ modules/clock/clock.h
@@ -5,4 +5,6 @@ public:
     const char* Name() const override { return "Clock"; }
 
     void Draw(IDirect3DDevice9* device) override;
+    void Initialize(ImGuiContext* ctx) override;
+    void Terminate() override;
 };
