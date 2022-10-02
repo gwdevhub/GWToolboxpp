66cc4d9fb5fcea4e20595eb4eb6146d9effe5075
DubbleClick <m@pyc.ac>
2022-10-02 22:46:15 +0200

    sort includes

--- GWToolboxdll/Modules/GameSettings.cpp
+++ GWToolboxdll/Modules/GameSettings.cpp
@@ -1,46 +1,47 @@
 #include "stdafx.h"
 
-#include <GWCA/Utilities/Scanner.h>
 #include <GWCA/Utilities/MemoryPatcher.h>
-
-#include <GWCA/GameContainers/Array.h>
-#include <GWCA/GameContainers/GamePos.h>
+#include <GWCA/Utilities/Scanner.h>
 
 #include <GWCA/Constants/AgentIDs.h>
 #include <GWCA/Constants/Constants.h>
 #include <GWCA/Constants/Maps.h>
 #include <GWCA/Constants/Skills.h>
 
-#include <GWCA/GameEntities/Item.h>
-#include <GWCA/GameEntities/Party.h>
-#include <GWCA/GameEntities/Skill.h>
-#include <GWCA/GameEntities/Agent.h>
-#include <GWCA/GameEntities/Player.h>
-#include <GWCA/GameEntities/Friendslist.h>
-#include <GWCA/GameEntities/Guild.h>
-#include <GWCA/GameEntities/Quest.h>
-#include <GWCA/GameEntities/Camera.h>
-#include <GWCA/GameEntities/Map.h>
-#include <GWCA/GameEntities/Title.h>
-#include <GWCA/GameEntities/Hero.h>
-
 #include <GWCA/Context/GuildContext.h>
+#include <GWCA/Context/PartyContext.h>
 #include <GWCA/Context/WorldContext.h>
 
-#include <GWCA/Managers/UIMgr.h>
-#include <GWCA/Managers/MapMgr.h>
-#include <GWCA/Managers/StoCMgr.h>
-#include <GWCA/Managers/PartyMgr.h>
+#include <GWCA/GameContainers/Array.h>
+#include <GWCA/GameContainers/GamePos.h>
+
+#include <GWCA/GameEntities/Agent.h>
+#include <GWCA/GameEntities/Camera.h>
+#include <GWCA/GameEntities/Friendslist.h>
+#include <GWCA/GameEntities/Guild.h>
+#include <GWCA/GameEntities/Hero.h>
+#include <GWCA/GameEntities/Item.h>
+#include <GWCA/GameEntities/Map.h>
+#include <GWCA/GameEntities/Party.h>
+#include <GWCA/GameEntities/Player.h>
+#include <GWCA/GameEntities/Quest.h>
+#include <GWCA/GameEntities/Skill.h>
+#include <GWCA/GameEntities/Title.h>
+
+#include <GWCA/Managers/AgentMgr.h>
 #include <GWCA/Managers/CameraMgr.h>
-#include <GWCA/Managers/MemoryMgr.h>
+#include <GWCA/Managers/ChatMgr.h>
+#include <GWCA/Managers/EffectMgr.h>
 #include <GWCA/Managers/FriendListMgr.h>
 #include <GWCA/Managers/GameThreadMgr.h>
-#include <GWCA/Managers/SkillbarMgr.h>
-#include <GWCA/Managers/EffectMgr.h>
-#include <GWCA/Managers/ChatMgr.h>
-#include <GWCA/Managers/AgentMgr.h>
-#include <GWCA/Managers/PlayerMgr.h>
 #include <GWCA/Managers/ItemMgr.h>
+#include <GWCA/Managers/MapMgr.h>
+#include <GWCA/Managers/MemoryMgr.h>
+#include <GWCA/Managers/PartyMgr.h>
+#include <GWCA/Managers/PlayerMgr.h>
+#include <GWCA/Managers/SkillbarMgr.h>
+#include <GWCA/Managers/StoCMgr.h>
+#include <GWCA/Managers/UIMgr.h>
 
 #include <GWCA/Utilities/Hooker.h>
 
@@ -51,17 +52,14 @@
 #include <Windows/StringDecoderWindow.h>
 #endif
 
-#include "GWCA/Context/PartyContext.h"
-
-#include <Modules/GameSettings.h>
 #include <Modules/ChatSettings.h>
 #include <Modules/DialogModule.h>
+#include <Modules/GameSettings.h>
 
-#include <Logger.h>
-#include <GWToolbox.h>
-#include <Timer.h>
 #include <Color.h>
 #include <hidusage.h>
+#include <Logger.h>
+#include <Timer.h>
 
 #pragma warning(disable : 6011)
 
