414d6c4505e09e9a67580fb8a1f695a49f2a86ec
Jon <>
2022-10-12 18:43:58 +0700

    Fixed potential crash when clicking hom achievements

--- GWToolboxdll/Windows/CompletionWindow.cpp
+++ GWToolboxdll/Windows/CompletionWindow.cpp
@@ -753,7 +753,12 @@ IDirect3DTexture9* ItemAchievement::GetMissionImage()
     return nullptr;
 }
 void ItemAchievement::OnClick() {
-    GuiUtils::OpenWiki(name.wstring());
+    wchar_t* buf = new wchar_t[name.wstring().size() + 1];
+    wcscpy(buf, name.wstring().c_str());
+    GW::GameThread::Enqueue([buf]() {
+        GuiUtils::OpenWiki(buf);
+        delete[] buf;
+        });
 }
 
 IDirect3DTexture9* PvESkill::GetMissionImage()
