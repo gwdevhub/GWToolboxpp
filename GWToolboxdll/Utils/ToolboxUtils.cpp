019952e29df36d755f7169432087220b24706fe3
Jon <>
2022-10-20 21:59:42 +0700

    Added functions to toolbox utils

--- GWToolboxdll/Utils/ToolboxUtils.cpp
+++ GWToolboxdll/Utils/ToolboxUtils.cpp
@@ -2,18 +2,33 @@
 
 #include <GWCA/Context/CharContext.h>
 #include <GWCA/Context/WorldContext.h>
+#include <GWCA/Context/PartyContext.h>
+
+#include <GWCA/GameEntities/Agent.h>
+#include <GWCA/GameEntities/Party.h>
+#include <GWCA/GameEntities/Player.h>
+#include <GWCA/GameEntities/Hero.h>
 
 #include <GWCA/Managers/PlayerMgr.h>
-#include <GWCA/Packets/StoC.h>
+#include <GWCA/Managers/PartyMgr.h>
+#include <GWCA/Managers/AgentMgr.h>
+#include <GWCA/Managers/MapMgr.h>
 
-#include <GWCA/GameEntities/Player.h>
+#include <GWCA/Packets/Opcodes.h>
+#include <GWCA/Packets/StoC.h>
 
 #include <Utils/GuiUtils.h>
 
 #include "ToolboxUtils.h"
-#include <GWCA/Packets/Opcodes.h>
 
 namespace ToolboxUtils {
+    bool IsOutpost() {
+        return GW::Map::GetInstanceType() == GW::Constants::InstanceType::Outpost;
+    }
+    bool IsExplorable() {
+        return GW::Map::GetInstanceType() == GW::Constants::InstanceType::Explorable;
+    }
+
     GW::Player* GetPlayerByName(const wchar_t* _name) {
         if (!_name) return NULL;
         std::wstring name = GuiUtils::SanitizePlayerName(_name);
@@ -67,5 +82,128 @@ namespace ToolboxUtils {
             return GetPlayerName(((uint32_t*)packet)[1]);
         }
         return L"";
+    }
+    GW::Player* GetPlayerByAgentId(uint32_t agent_id, GW::AgentLiving** info_out) {
+        const auto agent = static_cast<GW::AgentLiving*>(GW::Agents::GetAgentByID(agent_id));
+        if (!(agent && agent->GetIsLivingType() && agent->IsPlayer()))
+            return nullptr;
+        if (info_out)
+            *info_out = agent;
+        return GW::PlayerMgr::GetPlayerByID(agent->login_number);
+
+    }
+
+    GW::HeroInfo* GetHeroInfo(uint32_t hero_id) {
+        auto w = GW::GetWorldContext();
+        if (!(w && w->hero_info.size()))
+            return nullptr;
+        for (auto& a : w->hero_info) {
+            if (a.hero_id == hero_id)
+                return &a;
+        }
+        return nullptr;
+    }
+    bool IsHenchman(uint32_t agent_id) {
+        if (!IsOutpost()) {
+            return IsHenchmanInParty(agent_id);
+        }
+        auto w = GW::GetWorldContext();
+        if (!(w && w->henchmen_agent_ids.size()))
+            return false;
+        for (auto a : w->henchmen_agent_ids) {
+            if (a == agent_id)
+                return true;
+        }
+        return false;
+    }
+    bool IsHero(uint32_t agent_id, GW::HeroInfo** info_out) {
+        if (!IsOutpost()) {
+            // NB: HeroInfo array is only populated in outposts
+            return IsHeroInParty(agent_id);
+        }
+        auto w = GW::GetWorldContext();
+        if (!(w && w->hero_info.size()))
+            return false;
+        for (auto& a : w->hero_info) {
+            if (a.agent_id == agent_id) {
+                if (info_out)
+                    *info_out = &a;
+                return true;
+            }
+        }
+        return false;
+    }
+
+
+    GW::Array<GW::PartyInfo*>* GetParties() {
+        auto* p = GW::GetPartyContext();
+        return p ? &p->parties : nullptr;
+    }
+
+    const GW::HenchmanPartyMember* GetHenchmanPartyMember(uint32_t agent_id, GW::PartyInfo** party_out) {
+        const auto* parties = GetParties();
+        if (!parties) return nullptr;
+        for (const auto party : *parties) {
+            for (const auto& p : party->henchmen) {
+                if (p.agent_id != agent_id)
+                    continue;
+                if (party_out)
+                    *party_out = party;
+                return &p;
+            }
+        }
+        return nullptr;
+    }
+    bool IsHenchmanInParty(uint32_t agent_id) {
+        GW::PartyInfo* party = nullptr;
+        return GetHenchmanPartyMember(agent_id, &party) && party == GW::PartyMgr::GetPartyInfo();
+    }
+
+    const GW::HeroPartyMember* GetHeroPartyMember(uint32_t agent_id, GW::PartyInfo** party_out) {
+        const auto* parties = GetParties();
+        if (!parties) return nullptr;
+        for (const auto party : *parties) {
+            for (const auto& p : party->heroes) {
+                if (p.agent_id != agent_id)
+                    continue;
+                if (party_out)
+                    *party_out = party;
+                return &p;
+            }
+        }
+        return nullptr;
+    }
+    bool IsHeroInParty(uint32_t agent_id) {
+        GW::PartyInfo* party = nullptr;
+        return GetHeroPartyMember(agent_id, &party) && party == GW::PartyMgr::GetPartyInfo();
+    }
+
+    const GW::PlayerPartyMember* GetPlayerPartyMember(uint32_t login_number, GW::PartyInfo** party_out) {
+        const auto* parties = GetParties();
+        if (!parties) return nullptr;
+        for (const auto party : *parties) {
+            for (const auto& p : party->players) {
+                if (p.login_number != login_number)
+                    continue;
+                if (party_out)
+                    *party_out = party;
+                return &p;
+            }
+        }
+        return nullptr;
+    }
+    bool IsPlayerInParty(uint32_t login_number) {
+        GW::PartyInfo* party = nullptr;
+        return GetHeroPartyMember(login_number, &party) && party == GW::PartyMgr::GetPartyInfo();
+    }
+
+    bool IsAgentInParty(uint32_t agent_id) {
+        const auto* party = GW::PartyMgr::GetPartyInfo();
+        if (!party)
+            return false;
+        if (IsHenchmanInParty(agent_id) || IsHeroInParty(agent_id))
+            return true;
+        const auto player = GetPlayerByAgentId(agent_id);
+        return player && IsPlayerInParty(player->player_number);
     }
 }
--- GWToolboxdll/Utils/ToolboxUtils.h
+++ GWToolboxdll/Utils/ToolboxUtils.h
@@ -2,6 +2,13 @@
 
 namespace GW {
     struct Player;
+    struct Agent;
+    struct AgentLiving;
+    struct HeroPartyMember;
+    struct PlayerPartyMember;
+    struct HenchmanPartyMember;
+    struct PartyInfo;
+    struct HeroInfo;
 
     template <typename T>
     class Array;
@@ -13,10 +20,22 @@ namespace GW {
 }
 
 namespace ToolboxUtils {
+    // Map
+
+    bool IsOutpost();
+    bool IsExplorable();
+
+    // Player
+
     // Return player name without guild tag or player number
     std::wstring GetPlayerName(uint32_t player_number = 0);
     // Looks for player whilst sanitising for comparison
     GW::Player* GetPlayerByName(const wchar_t* _name);
+    // Pass GW::AgentLiving** to also grab the Agent info for this player
+    GW::Player* GetPlayerByAgentId(uint32_t agent_id, GW::AgentLiving** info_out = nullptr);
+
+    // Messages
+
     // Get buffer from world context for incoming chat message
     GW::Array<wchar_t>* GetMessageBuffer();
     // Get encoded message from world context for incoming chat message
@@ -25,4 +44,23 @@ namespace ToolboxUtils {
     bool ClearMessageCore();
     // Find the name of the player related to the incoming packet e.g. trade partner, chat message sender
     const std::wstring GetSenderFromPacket(GW::Packet::StoC::PacketBase* packet);
+
+    // Heros
+    
+    GW::HeroInfo* GetHeroInfo(uint32_t hero_id);
+    bool IsHenchman(uint32_t agent_id);
+    bool IsHero(uint32_t agent_id, GW::HeroInfo** info_out = nullptr);
+   
+    // Party related
+    
+    // Find HenchmanPartyMember by agent_id, pass GW::PartyInfo** to also grab the party this henchman belongs to
+    const GW::HenchmanPartyMember* GetHenchmanPartyMember(uint32_t, GW::PartyInfo** = nullptr);
+    bool IsHenchmanInParty(uint32_t);
+    // Find HeroPartyMember by agent_id, pass GW::PartyInfo** to also grab the party this hero belongs to
+    const GW::HeroPartyMember* GetHeroPartyMember(uint32_t, GW::PartyInfo** = nullptr);
+    bool IsHeroInParty(uint32_t);
+    // Find PlayerPartyMember by agent_id, pass GW::PartyInfo** to also grab the party this player belongs to
+    const GW::PlayerPartyMember* GetPlayerPartyMember(uint32_t, GW::PartyInfo** = nullptr);
+    bool IsPlayerInParty(uint32_t);
+    bool IsAgentInParty(uint32_t);
 };
