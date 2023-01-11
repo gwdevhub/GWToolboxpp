56b647b986f0a936f37bfab8a3dc63ef4cc43c9c
Jon <>
2023-01-11 14:54:26 +0000

    Moved private functions and vars into private namespace Tidied calculation function using pointers Use timer macros for last duped Show window as visible when hiding isn't ticked

--- GWToolboxdll/Windows/DupingWindow.cpp
+++ GWToolboxdll/Windows/DupingWindow.cpp
@@ -14,46 +14,158 @@
 #include <Modules/Resources.h>
 #include <Modules/ToolboxSettings.h>
 #include <Windows/DupingWindow.h>
+namespace {
+    struct DupeInfo {
+        DupeInfo(GW::AgentID agent_id) : agent_id(agent_id) {};
+#include <Timer.h>
 
-void DupingWindow::Initialize() {
-    ToolboxWindow::Initialize();
+        GW::AgentID agent_id;
+        clock_t last_duped = 0;
+        size_t dupe_count = 0;
+    };
+
+    bool OrderDupeInfo(DupeInfo& a, DupeInfo& b) {
+        const GW::AgentLiving* agentA = GW::Agents::GetAgentByID(a.agent_id)->GetAsAgentLiving();
+        const GW::AgentLiving* agentB = GW::Agents::GetAgentByID(b.agent_id)->GetAsAgentLiving();
+
+        if (agentA->hp > 0.35f && agentB->hp > 0.35f) {
+            return agentA->hp_pips > agentB->hp_pips;
+        }
+
+        return agentA->hp > agentB->hp;
+    }
+    void DrawDuping(const char* label, std::vector<DupeInfo>& vec) {
+        if (vec.size() == 0) return;
+
+        ImGui::Spacing();
+        ImGui::Text(label);
+        if (ImGui::BeginTable(label, 5)) {
+
+            ImGui::TableSetupColumn("Selection", ImGuiTableColumnFlags_WidthFixed, 0, 0);
+            ImGui::TableSetupColumn("HP", ImGuiTableColumnFlags_WidthStretch, -1, 0);
+            ImGui::TableSetupColumn("Regen", ImGuiTableColumnFlags_WidthFixed, 70, 1);
+            ImGui::TableSetupColumn("Dupe Count", ImGuiTableColumnFlags_WidthFixed, 20, 2);
+            ImGui::TableSetupColumn("Last Duped", ImGuiTableColumnFlags_WidthFixed, 40, 3);
+
+            const GW::Agent* target = GW::Agents::GetTarget();
+
+            for (auto& dupe_info : vec) {
+                auto agent = GW::Agents::GetAgentByID(dupe_info.agent_id)->GetAsAgentLiving();
+                auto selected = target && target->agent_id == agent->agent_id;
+
+                ImGui::PushID(dupe_info.agent_id);
+                ImGui::TableNextRow();
+
+                ImGui::TableSetColumnIndex(0);
+
+                if (ImGui::Selectable("", selected, ImGuiSelectableFlags_SpanAllColumns | ImGuiSelectableFlags_AllowItemOverlap, ImVec2(0, 23))) {
+                    if (ImGui::IsMouseReleased(ImGuiMouseButton_Left)) {
+                        GW::GameThread::Enqueue([agent]() {
+                            GW::Agents::ChangeTarget(agent);
+                            });
+                    }
+                }
+
+                ImGui::TableSetColumnIndex(1);
+                ImGui::ProgressBar(agent->hp);
+
+                ImGui::TableSetColumnIndex(2);
+                ImGui::Text(std::string((int)std::ceil(agent->hp_pips / 0.001852), '>').c_str());
+
+                ImGui::TableSetColumnIndex(3);
+                ImGui::Text("%d", dupe_info.dupe_count);
+
+                ImGui::TableSetColumnIndex(4);
+                if (dupe_info.last_duped > 0) {
+                    int seconds_ago = (int)(TIMER_DIFF(dupe_info.last_duped) / CLOCKS_PER_SEC);
+                    if (seconds_ago < 5) {
+                        ImGui::PushStyleColor(ImGuiCol_Text, Colors::ARGB(205, 102, 153, 230));
+                    }
+
+                    auto div = std::div(seconds_ago, 60);
+                    ImGui::Text("%d:%02d", div.quot, div.rem);
+
+                    if (seconds_ago < 5) {
+                        ImGui::PopStyleColor();
+                    }
+                }
+                else {
+                    ImGui::Text("-");
+                }
+
+                ImGui::PopID();
+            }
+
+            ImGui::EndTable();
+        }
+    }
+
+    std::vector<DupeInfo> souls{};
+    std::vector<DupeInfo> waters{};
+    std::vector<DupeInfo> minds{};
+
+    std::set<GW::AgentID> all_souls;
+    std::set<GW::AgentID> all_waters;
+    std::set<GW::AgentID> all_minds;
+
+    void ClearDupes() {
+        souls.clear();
+        waters.clear();
+        minds.clear();
+        all_souls.clear();
+        all_waters.clear();
+        all_minds.clear();
+    }
+
+    float range = 1600.0f;
+    bool hide_when_nothing = true;
+    bool show_souls_counter = true;
+    bool show_waters_counter = true;
+    bool show_minds_counter = true;
+    float souls_threshhold = 0.6f;
+    float waters_threshhold = 0.5f;
+    float minds_threshhold = 0.0f;
 }
 
 void DupingWindow::Terminate() {
-    souls.clear();
     ToolboxWindow::Terminate();
-}
-
-bool DupingWindow::OrderDupeInfo(DupeInfo& a, DupeInfo& b) {
-    const GW::AgentLiving* agentA = GW::Agents::GetAgentByID(a.agent_id)->GetAsAgentLiving();
-    const GW::AgentLiving* agentB = GW::Agents::GetAgentByID(b.agent_id)->GetAsAgentLiving();
-
-    if (agentA->hp > 0.35f && agentB->hp > 0.35f) {
-        return agentA->hp_pips > agentB->hp_pips;
-    }
-
-    return agentA->hp > agentB->hp;
+    ClearDupes();
 }
 
 void DupingWindow::Draw(IDirect3DDevice9* device) {
     UNREFERENCED_PARAMETER(device);
-    if (!visible || GW::Map::GetMapID() != GW::Constants::MapID::Domain_of_Anguish) return;
-    if (hide_when_nothing && GW::Map::GetInstanceType() != GW::Constants::InstanceType::Explorable) return;
-
-    // ==== Calculate the data ====
-    const GW::AgentArray* agents = GW::Agents::GetAgentArray();
-    const GW::Agent* player = GW::Agents::GetPlayer();
-    if (!agents || !player) return;
-
-    auto now = duration_cast<std::chrono::milliseconds>(std::chrono::system_clock::now().time_since_epoch()).count();
-    std::unordered_map<GW::AgentID, const GW::AgentLiving*> all_souls;
-    std::unordered_map<GW::AgentID, const GW::AgentLiving*> all_waters;
-    std::unordered_map<GW::AgentID, const GW::AgentLiving*> all_minds;
+    if (!visible) 
+        return;
+    bool is_in_doa = (GW::Map::GetMapID() == GW::Constants::MapID::Domain_of_Anguish && GW::Map::GetInstanceType() == GW::Constants::InstanceType::Explorable);
+    if (hide_when_nothing && !is_in_doa) 
+        return;
 
     const float sqr_range = range * range;
     int soul_count = 0;
     int water_count = 0;
     int mind_count = 0;
+    float threshold = .0f;
+    std::set<GW::AgentID>* all_agents_of_type = nullptr;
+    std::vector<DupeInfo>* duped_agents_of_type = nullptr;
+    const GW::AgentArray* agents = nullptr; 
+    const GW::Agent* player = nullptr;
+
+    // ==== Calculate the data ====
+    if (!is_in_doa) {
+        ClearDupes();
+        goto done_calculation;
+    }
+
+    all_souls.clear();
+    all_waters.clear();
+    all_minds.clear();
+
+    agents = GW::Agents::GetAgentArray();
+    player = agents ? GW::Agents::GetPlayer() : nullptr;
+    
+    if (!player)
+        goto done_calculation;
+
     for (auto* agent : *agents) {
         const GW::AgentLiving* living = agent ? agent->GetAsAgentLiving() : nullptr;
 
@@ -63,85 +175,59 @@ void DupingWindow::Draw(IDirect3DDevice9* device) {
         switch (living->player_number) {
             case GW::Constants::ModelID::DoA::SoulTormentor:
             case GW::Constants::ModelID::DoA::VeilSoulTormentor:
+                all_agents_of_type = &all_souls;
+                duped_agents_of_type = &souls;
+                threshold = souls_threshhold;
                 soul_count++;
                 break;
             case GW::Constants::ModelID::DoA::WaterTormentor:
             case GW::Constants::ModelID::DoA::VeilWaterTormentor:
+                all_agents_of_type = &all_waters;
+                duped_agents_of_type = &waters;
+                threshold = waters_threshhold;
                 water_count++;
                 break;
             case GW::Constants::ModelID::DoA::MindTormentor:
             case GW::Constants::ModelID::DoA::VeilMindTormentor:
+                all_agents_of_type = &all_minds;
+                duped_agents_of_type = &minds;
+                threshold = minds_threshhold;
                 mind_count++;
                 break;
             default:
                 continue;
         }
 
-        bool is_duping = living->skill == static_cast<uint16_t>(GW::Constants::SkillID::Call_to_the_Torment);
+        
 
-        if (living->hp <= souls_threshhold
-            && (living->player_number == GW::Constants::ModelID::DoA::SoulTormentor
-                || living->player_number == GW::Constants::ModelID::DoA::VeilSoulTormentor)
-        ) {
-            all_souls.insert_or_assign(living->agent_id, living);
+        if (living->hp <= threshold) {
+            all_agents_of_type->insert(living->agent_id);
 
-            auto found_soul = std::find_if(souls.begin(), souls.end(), [living](DupeInfo& info) {
+            bool is_duping = living->skill == static_cast<uint16_t>(GW::Constants::SkillID::Call_to_the_Torment);
+
+            auto found_dupe_info = std::find_if(duped_agents_of_type->begin(), duped_agents_of_type->end(), [living](DupeInfo& info) {
                 return info.agent_id == living->agent_id;
             });
-
-            if (found_soul != souls.end()) {
-                if (is_duping && (*found_soul).last_duped < now - 5000) {
-                    (*found_soul).last_duped = now;
-                    (*found_soul).dupe_count += 1;
-                }
+            if (found_dupe_info == duped_agents_of_type->end()) {
+                duped_agents_of_type->push_back(living->agent_id);
+                found_dupe_info = duped_agents_of_type->end() - 1;
             }
-            else {
-                souls.push_back(DupeInfo(living->agent_id, is_duping ? now : 0, is_duping ? 1 : 0));
-            }
-        } else if (living->hp <= waters_threshhold
-            && (living->player_number == GW::Constants::ModelID::DoA::WaterTormentor
-                || living->player_number == GW::Constants::ModelID::DoA::VeilWaterTormentor)
-        ) {
-            all_waters.insert_or_assign(living->agent_id, living);
-
-            auto found_water = std::find_if(waters.begin(), waters.end(), [living](DupeInfo& info) {
-                return info.agent_id == living->agent_id;
-            });
-
-            if (found_water != waters.end()) {
-                if (is_duping && (*found_water).last_duped < now - 5000) {
-                    (*found_water).last_duped = now;
-                    (*found_water).dupe_count += 1;
-                }
-            }
-            else {
-                waters.push_back(DupeInfo(living->agent_id, is_duping ? now : 0, is_duping ? 1 : 0));
-            }
-        }  else if (living->hp <= minds_threshhold
-            && (living->player_number == GW::Constants::ModelID::DoA::MindTormentor
-                || living->player_number == GW::Constants::ModelID::DoA::VeilMindTormentor)
-        ) {
-            all_minds.insert_or_assign(living->agent_id, living);
-
-            auto found_mind = std::find_if(minds.begin(), minds.end(), [living](DupeInfo& info) {
-                return info.agent_id == living->agent_id;
-            });
-
-            if (found_mind != minds.end()) {
-                if (is_duping && (*found_mind).last_duped < now - 5000) {
-                    (*found_mind).last_duped = now;
-                    (*found_mind).dupe_count += 1;
-                }
-            }
-            else {
-                minds.push_back(DupeInfo(living->agent_id, is_duping ? now : 0, is_duping ? 1 : 0));
+            if (is_duping && TIMER_DIFF(found_dupe_info->last_duped) > 5000) {
+                found_dupe_info->last_duped = TIMER_INIT();
+                found_dupe_info->dupe_count += 1;
             }
         }
     }
 
-    std::erase_if(souls, [all_souls](auto& info) { return !all_souls.contains(info.agent_id); });
-    std::erase_if(waters, [all_waters](auto& info) { return !all_waters.contains(info.agent_id); });
-    std::erase_if(minds, [all_minds](auto& info) { return !all_minds.contains(info.agent_id); });
+    std::erase_if(souls, [](auto& info) { return !all_souls.contains(info.agent_id); });
+    std::erase_if(waters, [](auto& info) { return !all_waters.contains(info.agent_id); });
+    std::erase_if(minds, [](auto& info) { return !all_minds.contains(info.agent_id); });
+
+    std::sort(souls.begin(), souls.end(), &OrderDupeInfo);
+    std::sort(waters.begin(), waters.end(), &OrderDupeInfo);
+    std::sort(minds.begin(), minds.end(), &OrderDupeInfo);
+
+    done_calculation:
 
     if (hide_when_nothing
         && !(show_souls_counter && soul_count > 0)
@@ -152,9 +238,7 @@ void DupingWindow::Draw(IDirect3DDevice9* device) {
         && minds.size() == 0
     ) return;
 
-    std::sort(souls.begin(), souls.end(), &OrderDupeInfo);
-    std::sort(waters.begin(), waters.end(), &OrderDupeInfo);
-    std::sort(minds.begin(), minds.end(), &OrderDupeInfo);
+
 
     // ==== Draw the window ====
     ImGui::SetNextWindowCenter(ImGuiCond_FirstUseEver);
@@ -196,77 +280,11 @@ void DupingWindow::Draw(IDirect3DDevice9* device) {
             }
         }
 
-        DrawDuping("Souls", now, souls);
-        DrawDuping("Waters", now, waters);
-        DrawDuping("Minds", now, minds);
+        DrawDuping("Souls", souls);
+        DrawDuping("Waters", waters);
+        DrawDuping("Minds", minds);
     }
     ImGui::End();
-}
-
-void DupingWindow::DrawDuping(const char* label, long long now, std::vector<DupeInfo> vec) {
-    if (vec.size() == 0) return;
-
-    ImGui::Spacing();
-    ImGui::Text(label);
-    if (ImGui::BeginTable(label, 5)) {
-
-        ImGui::TableSetupColumn("Selection", ImGuiTableColumnFlags_WidthFixed, 0, 0);
-        ImGui::TableSetupColumn("HP", ImGuiTableColumnFlags_WidthStretch, -1, 0);
-        ImGui::TableSetupColumn("Regen", ImGuiTableColumnFlags_WidthFixed, 70, 1);
-        ImGui::TableSetupColumn("Dupe Count", ImGuiTableColumnFlags_WidthFixed, 20, 2);
-        ImGui::TableSetupColumn("Last Duped", ImGuiTableColumnFlags_WidthFixed, 40, 3);
-
-        const GW::Agent* target = GW::Agents::GetTarget();
-
-        for (auto& dupe_info : vec) {
-            auto agent = GW::Agents::GetAgentByID(dupe_info.agent_id)->GetAsAgentLiving();
-            auto selected = target && target->agent_id == agent->agent_id;
-
-            ImGui::PushID(dupe_info.agent_id);
-            ImGui::TableNextRow();
-
-            ImGui::TableSetColumnIndex(0);
-
-            if (ImGui::Selectable("", selected, ImGuiSelectableFlags_SpanAllColumns | ImGuiSelectableFlags_AllowItemOverlap, ImVec2(0, 23))) {
-                if (ImGui::IsMouseReleased(ImGuiMouseButton_Left)) {
-                    GW::GameThread::Enqueue([agent]() {
-                        GW::Agents::ChangeTarget(agent);
-                    });
-                }
-            }
-
-            ImGui::TableSetColumnIndex(1);
-            ImGui::ProgressBar(agent->hp);
-
-            ImGui::TableSetColumnIndex(2);
-            ImGui::Text(std::string((int)std::ceil(agent->hp_pips / 0.001852), '>').c_str());
-
-            ImGui::TableSetColumnIndex(3);
-            ImGui::Text("%d", dupe_info.dupe_count);
-
-            ImGui::TableSetColumnIndex(4);
-            if (dupe_info.last_duped > 0) {
-                int seconds_ago = (int)(now - dupe_info.last_duped) / 1000;
-                if (seconds_ago < 5) {
-                    ImGui::PushStyleColor(ImGuiCol_Text, Colors::ARGB(205, 102, 153, 230));
-                }
-
-                auto div = std::div(seconds_ago, 60);
-                ImGui::Text("%d:%02d", div.quot, div.rem);
-
-                if (seconds_ago < 5) {
-                    ImGui::PopStyleColor();
-                }
-            }
-            else {
-                ImGui::Text("-");
-            }
-
-            ImGui::PopID();
-        }
-
-        ImGui::EndTable();
-    }
 }
 
 void DupingWindow::DrawSettingInternal() {
--- GWToolboxdll/Windows/DupingWindow.h
+++ GWToolboxdll/Windows/DupingWindow.h
@@ -17,7 +17,6 @@ public:
     const char* Name() const override { return "Duping"; }
     const char* Icon() const override { return ICON_FA_COPY; }
 
-    void Initialize() override;
     void Terminate() override;
 
     // Update. Will always be called every frame.
@@ -29,28 +28,4 @@ public:
     void DrawSettingInternal() override;
     void LoadSettings(ToolboxIni* ini) override;
     void SaveSettings(ToolboxIni* ini) override;
-
-private:
-    struct DupeInfo {
-        DupeInfo(GW::AgentID agent_id, long long last_duped, int dupe_count) : agent_id(agent_id), last_duped(last_duped), dupe_count(dupe_count) {};
-
-        GW::AgentID agent_id;
-        long long last_duped;
-        int dupe_count;
-    };
-
-    static bool OrderDupeInfo(DupeInfo& a, DupeInfo& b);
-    void DrawDuping(const char* label, long long now, std::vector<DupeInfo> vec);
-
-    std::vector<DupeInfo> souls{};
-    std::vector<DupeInfo> waters{};
-    std::vector<DupeInfo> minds{};
-    float range = 1600.0f;
-    bool hide_when_nothing = true;
-    bool show_souls_counter = true;
-    bool show_waters_counter = true;
-    bool show_minds_counter = true;
-    float souls_threshhold = 0.6f;
-    float waters_threshhold = 0.5f;
-    float minds_threshhold = 0.0f;
 };
