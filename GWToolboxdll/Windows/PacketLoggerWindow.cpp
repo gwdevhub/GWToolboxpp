33632e8a950f8ba9f4c6a463bdb868beaaddfb21
Jon <>
2023-01-15 19:52:36 +0000

    Fixed packet logger signature

--- GWToolboxdll/Windows/PacketLoggerWindow.cpp
+++ GWToolboxdll/Windows/PacketLoggerWindow.cpp
@@ -168,10 +168,7 @@ static void InitStoC()
     };
 
     uintptr_t StoCHandler_Addr;
-    // @Replaced: Data need to figure out relocation
-    // @Remark: Same pattern as CtoGSObjectPtr in CtoSMgr.cpp
-    uintptr_t address = GW::Scanner::Find(
-        "\xC3\xA1\x00\x00\x00\x00\x85\xC0\x74\xF1", "xx????xxxx", +2);
+    uintptr_t address = GW::Scanner::Find("\x75\x04\x33\xC0\x5D\xC3\x8B\x41\x08\xA8\x01\x75", "xxxxxxxxxxxx", -6);
     StoCHandler_Addr = *(uintptr_t*)address;
 
     GameServer** addr = (GameServer * *)StoCHandler_Addr;
