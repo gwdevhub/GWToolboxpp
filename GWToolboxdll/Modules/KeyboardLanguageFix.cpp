423026d5fc1055d44b278ffd4a42dc17e9f018d5
Jon <>
2023-02-01 22:59:55 +0000

    Testing with local keyboard language to see if it borks up

--- GWToolboxdll/Modules/KeyboardLanguageFix.cpp
+++ GWToolboxdll/Modules/KeyboardLanguageFix.cpp
@@ -22,24 +22,5 @@ void KeyboardLanguageFix::Initialize() {
         Log::Log("Failed to intercept keyboard layout override; Guild Wars has already set the keyboard language");
         return;
     }
-    HKL loaded_languages[0xff];
-    int keyboard_languages_count = GetKeyboardLayoutList(_countof(loaded_languages), loaded_languages);
-    if (!keyboard_languages_count) {
-        Log::Error("Failed to GetKeyboardLayoutList, GetLastError = %#08x", GetLastError());
-        return;
-    }
-    // Ensure that en-US is available for this system. Side effect is that it adds the layout to the OS, which is naughty
-    *address = LoadKeyboardLayoutA("00000409", 0);
-    int current_keyboard_layout_count = GetKeyboardLayoutList(_countof(loaded_languages), loaded_languages);
-    if (!current_keyboard_layout_count) {
-        Log::Error("Failed to GetKeyboardLayoutList, GetLastError = %#08x", GetLastError());
-        return;
-    }
-    // If the en US layout wasn't previously present, remove it.
-    if (current_keyboard_layout_count != keyboard_languages_count) {
-        if (!UnloadKeyboardLayout(*address)) {
-            Log::Error("Failed to UnloadKeyboardLayout, GetLastError = %#08x", GetLastError());
-            return;
-        }
-    }
+    *address = GetKeyboardLayout(0);
 }
