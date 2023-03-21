e2ca225f7e2e8605b1890b32f18ad53d1ee81d65
Jon <>
2023-03-21 23:00:23 +0000

    Removed remote dependency for nfd

--- cmake/nativefiledialog.cmake
+++ cmake/nativefiledialog.cmake
@@ -1,17 +1,4 @@
 include_guard()
-include(FetchContent)
-
-FetchContent_Declare(
-    nativefiledialog
-    GIT_REPOSITORY https://github.com/mlabbe/nativefiledialog.git
-    GIT_TAG release_116)
-FetchContent_GetProperties(nativefiledialog)
-if (nativefiledialog_POPULATED)
-    return()
-endif()
-
-FetchContent_Populate(nativefiledialog)
 
 add_library(nativefiledialog INTERFACE)
-	
-target_include_directories(nativefiledialog INTERFACE "${nativefiledialog_SOURCE_DIR}/src")
+target_include_directories(nativefiledialog INTERFACE "${PROJECT_SOURCE_DIR}/dependencies/nativefiledialog")
