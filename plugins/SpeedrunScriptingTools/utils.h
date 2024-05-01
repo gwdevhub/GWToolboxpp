698f980cc287397c29532c001c9763170caca915
Jabor <lja@posteo.de>
2024-05-01 13:51:42 +0200

    Target Self

--- plugins/SpeedrunScriptingTools/utils.h
+++ plugins/SpeedrunScriptingTools/utils.h
@@ -27,7 +27,7 @@ std::string makeHotkeyDescription(long keyData, long modifier);
 
 enum class Class : int { Any, Warrior, Ranger, Monk, Necro, Mesmer, Elementalist, Assassin, Ritualist, Paragon, Dervish };
 enum class Status : int { Any, Dead, Alive };
-enum class AgentType : int { Any, PartyMember, Friendly, Hostile };
+enum class AgentType : int { Any, Self, PartyMember, Friendly, Hostile };
 enum class Sorting : int { AgentId, ClosestToPlayer, FurthestFromPlayer, ClosestToTarget, FurthestFromTarget, LowestHp, HighestHp };
 enum class HexedStatus : int { Any, NotHexed, Hexed };
 
