bebb5e839a0473a61fa7e057e2bfc21732b2801d
Alex Macocian <amacocian@yahoo.com>
2024-05-09 01:03:25 +0200

    Improve caching Rely on modified time Strip extra code

--- cmake/openssl.cmake
+++
@@ -1,9 +0,0 @@
-include_guard()
-
-set(OPENSSL_FOLDER "${PROJECT_SOURCE_DIR}/Dependencies/openssl")
-
-add_library(openssl INTERFACE)
-target_include_directories(openssl INTERFACE "${OPENSSL_FOLDER}/include")
-target_link_libraries(openssl INTERFACE
-    "${OPENSSL_FOLDER}/lib/libssl.lib"
-    "${OPENSSL_FOLDER}/lib/libcrypto.lib")
