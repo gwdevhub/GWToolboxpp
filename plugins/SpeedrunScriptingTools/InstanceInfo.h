f6945f0c861bc8fb4fdaae195feaee77bfc1c2a4
Jabor <lja@posteo.de>
2024-05-19 15:59:06 +0200

    Fix partymemberstatus condition

--- plugins/SpeedrunScriptingTools/InstanceInfo.h
+++ plugins/SpeedrunScriptingTools/InstanceInfo.h
@@ -21,12 +21,10 @@ public:
     }
     QuestStatus getQuestStatus(GW::Constants::QuestID);
     std::string getDecodedName(GW::AgentID);
-    void update();
 
     bool canPopAgent() const;
     bool hasMinipetPopped() const;
     int getInstanceId() const { return instanceId; }
-    const std::unordered_map<GW::AgentID, std::string>& getPlayerNames() const { return playerDecodedNames; }
     void storeTarget(const GW::AgentLiving* agent, int storageId);
     const GW::AgentLiving* retrieveTarget(int storageId) const;
 
@@ -40,7 +38,6 @@ private:
     InstanceInfo() = default;
     std::unordered_map<GW::Constants::QuestID, QuestStatus> questStatus;
     std::unordered_map<GW::AgentID, std::wstring> decodedNames;
-    std::unordered_map<GW::AgentID, std::string> playerDecodedNames;
     std::unordered_map<int, GW::AgentID> storedTargets;
     int instanceId = 0;
     MiniPetStatus mpStatus;
