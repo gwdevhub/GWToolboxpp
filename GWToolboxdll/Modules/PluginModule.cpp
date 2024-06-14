f5a26a5775563d489dac844a01c3276703433a43
DubbleClick <marc.henderkes@fu-berlin.de>
2024-06-14 15:26:41 +0200

    Revert "6.21"

--- GWToolboxdll/Modules/PluginModule.cpp
+++ GWToolboxdll/Modules/PluginModule.cpp
@@ -227,10 +227,9 @@ void PluginModule::Draw(IDirect3DDevice9* device)
     static bool message_displayed = false;
     if (!plugins_loaded.empty() && !message_displayed) {
         WriteChat(GW::Chat::Channel::CHANNEL_GWCA2,
-                  L"<c=#FFFF00>Plugins detected, these may be unsafe to use and are not officially supported by GWToolbox++ developers.\n"
+                  L"<c=#FF0000>Plugins detected, these may be unsafe to use and are not officially supported by GWToolbox++ developers.\n"
                   "Use at your own risk if you trust the author.\n"
-                  "Do not report bugs that occur while you play with plugins.</c>\n"
-                  "<c=#FF0000>Plugins are NOT permitted by ArenaNet.</c>",
+                  "Do not report bugs that occur while you play with plugins.</c>",
                   L"GWToolbox++");
         message_displayed = true;
     }
