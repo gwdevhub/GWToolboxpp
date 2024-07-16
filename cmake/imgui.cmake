4b660360a2345a9eac462be6492bf39bb07d123e
DubbleClick <m@pyc.ac>
2024-07-16 20:47:35 +0200

    make cmake run even without successful patch (because it fails when it's already patched...)

--- cmake/imgui.cmake
+++ cmake/imgui.cmake
@@ -1,21 +1,31 @@
 include_guard()
-include(gwca)
 include(FetchContent)
-
-set(imgui_patch git apply ${PROJECT_SOURCE_DIR}/cmake/patches/imgui_transparent_viewports.patch)
 
 FetchContent_Declare(
     imgui
     GIT_REPOSITORY https://github.com/ocornut/imgui.git
     GIT_TAG v1.90.9-docking
-	PATCH_COMMAND ${imgui_patch}
-	)
+    )
 FetchContent_GetProperties(imgui)
 if (imgui_POPULATED)
     return()
 endif()
 
 FetchContent_Populate(imgui)
+
+# Apply the patch without halting the build on failure, in case it's already applied
+execute_process(
+    COMMAND git apply ${PROJECT_SOURCE_DIR}/cmake/patches/imgui_transparent_viewports.patch
+    WORKING_DIRECTORY ${imgui_SOURCE_DIR}
+    RESULT_VARIABLE patch_result
+    ERROR_VARIABLE patch_error
+    OUTPUT_VARIABLE patch_output
+)
+
+if (NOT patch_result EQUAL 0)
+    message(WARNING "Failed to apply patch: ${patch_error}")
+    message(WARNING "Patch output: ${patch_output}")
+endif()
 
 add_library(imgui)
 set(SOURCES
@@ -28,12 +38,12 @@ set(SOURCES
     "${imgui_SOURCE_DIR}/imgui_widgets.cpp"
     # we copied (and modified) impl_imgui_dx9.h/cpp files under GWToolboxdll
     # we copied (and modified) imgui_impl_win32.h/cpp files under GWToolboxdll
-    )
+)
 source_group(TREE "${imgui_SOURCE_DIR}" FILES ${SOURCES})
 target_sources(imgui PRIVATE ${SOURCES})
 target_include_directories(imgui PUBLIC
-	"${PROJECT_SOURCE_DIR}/Dependencies"
-	"${imgui_SOURCE_DIR}"
+    "${PROJECT_SOURCE_DIR}/Dependencies"
+    "${imgui_SOURCE_DIR}"
 	)
 target_compile_definitions(imgui PUBLIC 
     IMGUI_USER_CONFIG="${PROJECT_SOURCE_DIR}/GWToolboxdll/imconfig.h")
