66072dc49ae1835129f6ca03eaeab53ef9f56442
Alex Macocian <amacocian@yahoo.com>
2024-07-28 12:36:32 +0200

    Colored quest markers Adjust SymbolRenderer to use a pixel and vertex shader

--- GWToolboxdll/Widgets/Minimap/SymbolsRenderer.h
+++ GWToolboxdll/Widgets/Minimap/SymbolsRenderer.h
@@ -15,7 +15,7 @@ public:
 
 private:
     void Initialize(IDirect3DDevice9* device) override;
-
+    bool ConfigureProgrammablePipeline(IDirect3DDevice9* device);
     Color color_quest = 0;
     Color color_other_quests = 0;
     Color color_north = 0;
