3d65574c811e07eb7624ae7ed173cfb859172fb4
Jabor <lja@posteo.de>
2024-09-14 23:46:41 +0200

    Bump version... again

--- plugins/SpeedrunScriptingTools/SpeedrunScriptingTools.cpp
+++ plugins/SpeedrunScriptingTools/SpeedrunScriptingTools.cpp
@@ -434,7 +434,7 @@ void SpeedrunScriptingTools::DrawSettings()
     ImGui::SameLine();
     ImGui::Checkbox("Block hotkey keys even if conditions not met", &alwaysBlockHotkeyKeys);
 
-    ImGui::Text("Version 1.6.2. For new releases, feature requests and bug reports check out");
+    ImGui::Text("Version 1.6.3. For new releases, feature requests and bug reports check out");
     ImGui::SameLine();
 
     constexpr auto discordInviteLink = "https://discord.gg/ZpKzer4dK9";
