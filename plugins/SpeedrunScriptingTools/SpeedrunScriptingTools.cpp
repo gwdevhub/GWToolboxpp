4a35a44d2222fb07b06062bbfb49164519b62550
Jabor <lja@posteo.de>
2024-10-16 20:14:19 +0200

    Store and restore targets with ID

--- plugins/SpeedrunScriptingTools/SpeedrunScriptingTools.cpp
+++ plugins/SpeedrunScriptingTools/SpeedrunScriptingTools.cpp
@@ -676,7 +676,7 @@ void SpeedrunScriptingTools::DrawSettings()
     ImGui::SameLine();
     ImGui::Checkbox("Block hotkey keys even if conditions not met", &alwaysBlockHotkeyKeys);
 
-    ImGui::Text("Version 2.0.1. For new releases, feature requests and bug reports check out");
+    ImGui::Text("Version 2.0.1-storeRestoreTargets. For new releases, feature requests and bug reports check out");
     ImGui::SameLine();
 
     constexpr auto discordInviteLink = "https://discord.gg/ZpKzer4dK9";
