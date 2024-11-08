7ba2748b421941245af10c8808075c7f4272327c
Jon <jon.r+gh@live.com>
2024-11-08 19:56:56 +0000

    Assertion check

--- GWToolboxdll/Modules/ItemDescriptionHandler.cpp
+++ GWToolboxdll/Modules/ItemDescriptionHandler.cpp
@@ -105,11 +105,15 @@ std::wstring ItemDescriptionHandler::GetItemEncNameWithoutMods(const GW::Item* _
 }
 void ItemDescriptionHandler::Initialize()
 {
-    GetItemDescription_Func = reinterpret_cast<GetItemDescriptionCallback>(GW::Scanner::Find("\x8b\xc3\x25\xfd\x00\x00\x00\x3c\xfd", "xxxxxxxxx", -0x5f));
-    if (GetItemDescription_Func) {
+    const auto address = GW::Scanner::Find("\x8b\xc3\x25\xfd\x00\x00\x00\x3c\xfd", "xxxxxxxxx", -0x5f);
+    if (GW::Scanner::IsValidPtr(address, GW::Scanner::Section::TEXT)) {
+        GetItemDescription_Func = (GetItemDescriptionCallback)address;
         GW::HookBase::CreateHook((void**) & GetItemDescription_Func, OnGetItemDescription, (void**)&GetItemDescription_Ret);
         GW::HookBase::EnableHooks(GetItemDescription_Func);
     }
+#ifdef _DEBUG
+    ASSERT(GetItemDescription_Func);
+#endif
 }
 
 void ItemDescriptionHandler::SignalTerminate()
