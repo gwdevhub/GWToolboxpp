0eac89888c9968090a61459e0e01a731c47cbad0
DubbleClick <m@pyc.ac>
2024-11-12 09:31:02 +0700

    work on vcpkg integration

--- GWToolboxdll/Modules/ToastNotifications.cpp
+++ GWToolboxdll/Modules/ToastNotifications.cpp
@@ -19,7 +19,7 @@
 #include <GWCA/Managers/PartyMgr.h>
 #include <GWCA/Managers/PlayerMgr.h>
 
-#include <wintoastlib.h>
+#include <wintoast/wintoastlib.h>
 #include <Utils/GuiUtils.h>
 #include <Utils/ToolboxUtils.h>
 #include <Defines.h>
@@ -341,9 +341,9 @@ void ToastNotifications::Toast::toastDismissed(WinToastDismissalReason) const
     TriggerToastCallback(this, false);
 };
 
-void ToastNotifications::Toast::toastFailed() const
+void ToastNotifications::Toast::toastFailed(HRESULT err) const
 {
-    Log::Error("Failed to show toast");
+    Log::Error("Failed to show toast, error code: %d", err);
     TriggerToastCallback(this, false);
 }
 
---
+++ cmake/wintoast.cmake
@@ -0,0 +1,13 @@
+set(wintoast_FOLDER "${PROJECT_SOURCE_DIR}/Dependencies/wintoast/")
+
+add_library(wintoast)
+
+set(SOURCES
+    "${wintoast_FOLDER}/wintoastlib.h"
+    "${wintoast_FOLDER}/wintoastlib.cpp"
+    )
+    
+target_sources(wintoast PRIVATE ${SOURCES})
+target_include_directories(wintoast PUBLIC "${wintoast_FOLDER}")
+
+set_target_properties(wintoast PROPERTIES FOLDER "Dependencies/")
