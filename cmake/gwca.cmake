83c2cb72a2800e565413a08232e1fa558e1bcc91
Jon <jon.r+gh@live.com>
2024-12-12 08:45:18 +0000

    fixed cmake bugs

--- cmake/gwca.cmake
+++ cmake/gwca.cmake
@@ -6,7 +6,7 @@ find_package(minhook CONFIG REQUIRED)
 
 add_library(gwca SHARED IMPORTED GLOBAL)
 set_target_properties(gwca PROPERTIES
-    IMPORTED_IMPLIB "${GWCA_FOLDER}/bin/gwca.dll"
-    IMPORTED_LOCATION "${GWCA_FOLDER}/lib/gwca.lib"
+    IMPORTED_IMPLIB "${GWCA_FOLDER}/lib/gwca.lib"
+    IMPORTED_LOCATION "${GWCA_FOLDER}/bin/gwca.dll"
     INTERFACE_INCLUDE_DIRECTORIES "${GWCA_FOLDER}/include")
 target_link_libraries(gwca INTERFACE minhook::minhook)
