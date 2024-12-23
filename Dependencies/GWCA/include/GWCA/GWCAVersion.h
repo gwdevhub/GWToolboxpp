fffce24f1353776ed0b71830b8ad09f1a681f2b9
Jon <jon.r+gh@live.com>
2024-12-23 10:31:07 +0000

    Fixed crash viewing advanced map info

--- Dependencies/GWCA/include/GWCA/GWCAVersion.h
+++ Dependencies/GWCA/include/GWCA/GWCAVersion.h
@@ -2,9 +2,9 @@
 
 #define GWCA_VERSION_MAJOR 1
 #define GWCA_VERSION_MINOR 0
-#define GWCA_VERSION_PATCH 0
+#define GWCA_VERSION_PATCH 1
 #define GWCA_VERSION_BUILD 0
-#define GWCA_VERSION "1.0.0.0"
+#define GWCA_VERSION "1.0.1.0"
 
 namespace GWCA {
     constexpr int VersionMajor = GWCA_VERSION_MAJOR;
