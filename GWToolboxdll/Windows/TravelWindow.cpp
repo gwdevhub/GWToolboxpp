efd8bf5d9b27b14812119f5c171686bc1d9185c7
Khios <204761806+KhiosDev@users.noreply.github.com.>
2025-03-24 19:16:49 +0100

    Removed a check for PreSearing Region that was exiting out of finding the nearest outpost while using a Pre characater

--- GWToolboxdll/Windows/TravelWindow.cpp
+++ GWToolboxdll/Windows/TravelWindow.cpp
@@ -864,7 +864,7 @@ GW::Constants::MapID TravelWindow::GetNearestOutpost(const GW::Constants::MapID 
         if (!(IsValidOutpost(map_id) && GW::Map::GetMapInfo(map_id)->GetIsOnWorldMap()))
             continue;
         const auto map_info = GW::Map::GetMapInfo(map_id);
-        if (map_info->campaign != this_map->campaign || map_info->region == GW::Region_Presearing)
+        if (map_info->campaign != this_map->campaign)
             continue;
         //if ((map_info->flags & 0x5000000) != 0)
         //   continue; // e.g. "wrong" augury rock is map 119, no NPCs
