7f8cb68c7c47a55dd54540cfaa4430b28069061d
Jon <jon.r+gh@live.com>
2025-04-23 07:17:05 +0100

    Enable support for another 20 years

--- GWToolboxdll/Modules/GameSettings.cpp
+++ GWToolboxdll/Modules/GameSettings.cpp
@@ -1377,15 +1377,14 @@ namespace {
         while (EnumDisplaySettings(NULL, mode_num++, &dev_mode)) {
             max_refresh_rate = std::max(dev_mode.dmDisplayFrequency, max_refresh_rate);
         }
-        static const uint16_t static_max_refresh = static_cast<uint16_t>(max_refresh_rate);
 
         auto address = GW::Scanner::Find("\xeb\x05\xbe\xb4\x00\x00\x00", "xxxxxxx", 3);
-        if (!address || static_max_refresh == 180) return false;
-        max_frame_limit_patch.SetPatch(address, reinterpret_cast<const char*>(&static_max_refresh), sizeof(static_max_refresh));
+        if (!address || max_refresh_rate == 180) return false;
+        max_frame_limit_patch.SetPatch(address, reinterpret_cast<const char*>(&max_refresh_rate), sizeof(max_refresh_rate));
 
         address = GW::Scanner::Find("\x75\x05\xbe\xb4\x00\x00\x00", "xxxxxxx", 3);
         if (!address) return false;
-        default_frame_limit_patch.SetPatch(address, reinterpret_cast<const char*>(&static_max_refresh), sizeof(static_max_refresh));
+        default_frame_limit_patch.SetPatch(address, reinterpret_cast<const char*>(&max_refresh_rate), sizeof(max_refresh_rate));
 
         max_frame_limit_patch.TogglePatch(true);
         default_frame_limit_patch.TogglePatch(true);
