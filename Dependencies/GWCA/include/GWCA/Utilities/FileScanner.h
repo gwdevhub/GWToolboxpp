ee1fe698fdba57c487773fb080a7a922c4693c28
DubbleClick <m@pyc.ac>
2025-05-04 18:10:11 +0700

    merge origin into local

--- Dependencies/GWCA/include/GWCA/Utilities/FileScanner.h
+++ Dependencies/GWCA/include/GWCA/Utilities/FileScanner.h
@@ -9,13 +9,13 @@ class FileScanner
 public:
     static bool CreateFromPath(const wchar_t *path, FileScanner *result);
 
-
-
     FileScanner();
     FileScanner(const void *FileMapping, uintptr_t ImageBase, PIMAGE_SECTION_HEADER Sections, size_t Count);
+    FileScanner& operator=(FileScanner&& other) noexcept;
+    FileScanner& operator=(const FileScanner& other) noexcept;
     ~FileScanner();
 
-    void GetSectionAddressRange(GW::ScannerSection section, uintptr_t* start, uintptr_t* end);
+    void GetSectionAddressRange(GW::ScannerSection section, uintptr_t* start, uintptr_t* end) const;
     uintptr_t FindAssertion(const char* assertion_file, const char* assertion_msg, uint32_t line_number, int offset);
     uintptr_t FindInRange(const char* pattern, const char* mask, int offset, uint32_t start, uint32_t end);
     uintptr_t Find(const char* pattern, const char* mask, int offset, GW::ScannerSection section = GW::ScannerSection::Section_TEXT);
