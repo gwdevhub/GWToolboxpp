7494e2adafe0d5170520e10aff30c2c3b3d5f317
Jon <jon.r+gh@live.com>
2025-06-07 19:41:48 +0100

    Fixed crash setting vsync pref

--- Dependencies/GWCA/include/GWCA/GWCAVersion.h
+++ Dependencies/GWCA/include/GWCA/GWCAVersion.h
@@ -2,9 +2,9 @@
 
 #define GWCA_VERSION_MAJOR 1
 #define GWCA_VERSION_MINOR 1
-#define GWCA_VERSION_PATCH 8
+#define GWCA_VERSION_PATCH 9
 #define GWCA_VERSION_BUILD 0
-#define GWCA_VERSION "1.1.8.0"
+#define GWCA_VERSION "1.1.9.0"
 
 namespace GWCA {
     constexpr int VersionMajor = GWCA_VERSION_MAJOR;
