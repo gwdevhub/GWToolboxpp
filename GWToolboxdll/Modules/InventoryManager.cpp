c484146371f505178f6d0f2c620cfcaebdb3d5fc
Jon <jon.r+gh@live.com>
2025-06-12 11:08:51 +0100

    Fix bug preventing lesser salvage kits from working with salvage all after gw update

--- GWToolboxdll/Modules/InventoryManager.cpp
+++ GWToolboxdll/Modules/InventoryManager.cpp
@@ -1223,7 +1223,7 @@ void InventoryManager::ContinueSalvage()
             pending_salvage_at = TIMER_INIT();
         }
         // Auto accept "you can only salvage materials with a lesser salvage kit"
-        GW::UI::ButtonClick(GW::UI::GetChildFrame(GW::UI::GetFrameByLabel(L"Game"), 0x6, 0x62, 0x6));
+        GW::UI::ButtonClick(GW::UI::GetChildFrame(GW::UI::GetFrameByLabel(L"Game"), 0x6, 0x64, 0x6));
         return;
     }
     is_salvaging = false;
