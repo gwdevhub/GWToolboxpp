28d252d20a8eee3cf22053fed587d0b61fc659dc
Jon <jon.r+gh@live.com>
2025-12-24 00:32:09 +0000

    Fixed occasional desync of skill timers in some edge cases

--- Dependencies/GWCA/include/GWCA/GWCAVersion.h
+++ Dependencies/GWCA/include/GWCA/GWCAVersion.h
@@ -2,9 +2,9 @@
 
 #define GWCA_VERSION_MAJOR 2
 #define GWCA_VERSION_MINOR 1
-#define GWCA_VERSION_PATCH 2
+#define GWCA_VERSION_PATCH 3
 #define GWCA_VERSION_BUILD 0
-#define GWCA_VERSION "2.1.2.0"
+#define GWCA_VERSION "2.1.3.0"
 
 namespace GWCA {
     constexpr int VersionMajor = GWCA_VERSION_MAJOR;
