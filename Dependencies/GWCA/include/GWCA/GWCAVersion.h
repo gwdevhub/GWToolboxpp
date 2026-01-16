c2b3f09a78449c802a54f6805d46a24c14fa4bce
Jon <jon.r+gh@live.com>
2026-01-16 09:43:58 +0000

    Finally fixed the bug preventing gamepad cursor

--- Dependencies/GWCA/include/GWCA/GWCAVersion.h
+++ Dependencies/GWCA/include/GWCA/GWCAVersion.h
@@ -1,10 +1,10 @@
 #pragma once
 
 #define GWCA_VERSION_MAJOR 2
-#define GWCA_VERSION_MINOR 1
+#define GWCA_VERSION_MINOR 2
 #define GWCA_VERSION_PATCH 6
 #define GWCA_VERSION_BUILD 0
-#define GWCA_VERSION "2.1.6.0"
+#define GWCA_VERSION "2.2.6.0"
 
 namespace GWCA {
     constexpr int VersionMajor = GWCA_VERSION_MAJOR;
