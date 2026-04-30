7cd969501287cd1e6ebf6f03fe2eacbf33e80a70
Gunnar Beutner <gunnar@beutner.de>
2026-04-30 07:30:38 +0200

    Fix account inventory showing filtered count as total item count

--- GWToolboxdll/Windows/AccountInventoryWindow.cpp
+++ GWToolboxdll/Windows/AccountInventoryWindow.cpp
@@ -1869,7 +1869,7 @@ void AccountInventoryWindow::Draw(IDirect3DDevice9*)
     ImGui::SetNextItemWidth(300.f * font_scale);
     if (ImGui::InputText("###item_filter", item_filter_buf, _countof(item_filter_buf))) needs_sorting = true;
     ImGui::SameLine();
-    ImGui::Text("Filter   %d/%d Items", filtered_item_count, inventory_sorted.size());
+    ImGui::Text("Filter   %d/%d Items", filtered_item_count, inventory.size());
     ImGui::TableNextRow();
     ImGui::TableNextColumn();
 
