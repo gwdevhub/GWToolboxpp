8a673069987146b3ca7a727283a7ccd4c355de37
DubbleClick <m@pyc.ac>
2026-05-17 10:51:25 +0700

    supress warnings

--- build-clang.bat
+++ build-clang.bat
@@ -1,4 +1,4 @@
 @echo off
 call "%VCINSTALLDIR%\Auxiliary\Build\vcvars32.bat" || exit /b 1
-cmake --preset clang -DCMAKE_BUILD_TYPE=RelWithDebInfo || exit /b 1
+cmake --preset clang -DCMAKE_BUILD_TYPE=RelWithDebInfo --no-warn-unused-cli || exit /b 1
 cmake --build build-clang || exit /b 1
