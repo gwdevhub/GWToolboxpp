a7c5a33761be6bb55cb893fc5012b63aacf54ca5
DubbleClick <m@pyc.ac>
2026-06-21 16:02:36 +0700

    up gwca

--- Dependencies/GWCA/include/GWCA/GWCAVersion.h
+++ Dependencies/GWCA/include/GWCA/GWCAVersion.h
@@ -1,10 +1,10 @@
 #pragma once
 
 #define GWCA_VERSION_MAJOR 4
-#define GWCA_VERSION_MINOR 5
-#define GWCA_VERSION_PATCH 7
+#define GWCA_VERSION_MINOR 6
+#define GWCA_VERSION_PATCH 0
 #define GWCA_VERSION_BUILD 0
-#define GWCA_VERSION "4.5.7.0"
+#define GWCA_VERSION "4.6.0.0"
 
 namespace GWCA {
     constexpr int VersionMajor = GWCA_VERSION_MAJOR;
