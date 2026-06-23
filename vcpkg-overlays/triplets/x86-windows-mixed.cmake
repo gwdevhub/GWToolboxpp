7f28500db494856f19b17db57f27ba67d82207ae
DubbleClick <m@pyc.ac>
2026-06-23 23:39:59 +0700

    remove openmp dependency

--- vcpkg-overlays/triplets/x86-windows-mixed.cmake
+++ vcpkg-overlays/triplets/x86-windows-mixed.cmake
@@ -7,3 +7,8 @@ else()
     set(VCPKG_CRT_LINKAGE static)
     set(VCPKG_LIBRARY_LINKAGE static)
 endif()
+
+# we never run BC6H/BC7 software compression
+if(PORT STREQUAL "directxtex")
+    set(VCPKG_CMAKE_CONFIGURE_OPTIONS "-DBC_USE_OPENMP=OFF")
+endif()
