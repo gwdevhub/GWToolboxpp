7cac9352d214da7050e4125db287fe22273d7732
Marc <m@pyc.ac>
2026-08-19 22:58:35 +0200

    Revert "Drop CrashFixesModule now the client no longer trips the blockmap assert"

--- GWToolboxdll/GWToolbox.cpp
+++ GWToolboxdll/GWToolbox.cpp
@@ -33,6 +33,7 @@
 #include <Modules/CameraUnlockModule.h>
 #include <Modules/ChatCommands.h>
 #include <Modules/ChatSettings.h>
+#include <Modules/CrashFixesModule.h>
 #include <Modules/CrashHandler.h>
 #include <Modules/DialogModule.h>
 #include <Modules/GameSettings.h>
@@ -1283,6 +1284,7 @@ void GWToolbox::UpdateInitialising(float)
 
     Log::Log("Creating Modules\n");
     ToggleModule(CrashHandler::Instance());
+    ToggleModule(CrashFixesModule::Instance());
     ToggleModule(Resources::Instance());
     ToggleModule(ToolboxTheme::Instance());
     ToggleModule(ItemDescriptionHandler::Instance());
